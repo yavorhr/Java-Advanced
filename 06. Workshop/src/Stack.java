821ab3dd6d0e3df50c066e405b0b103becfe6a8f
yavorhr <76119513+yavorhr@users.noreply.github.com>
2022-05-03 12:06:58 +0300

    ArrayList and Stack custom implementations

---
+++ 06. Workshop/src/Stack.java
@@ -0,0 +1,69 @@
+import java.util.function.Consumer;
+
+public class Stack {
+
+    private static class Node {
+        private int element;
+        private Node prev;
+
+        private Node(int element) {
+            this.element = element;
+        }
+    }
+
+    private Node top;
+    private int size;
+
+    public Stack() {
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public void push(int element) {
+        Node newNode = new Node(element);
+        //add for first time
+        if (this.top == null) {
+            this.top = newNode;
+        } else {
+            newNode.prev = this.top;
+            this.top = newNode;
+        }
+        this.size++;
+    }
+
+    public int pop() {
+        ensureNonEmpty();
+        int result = this.top.element;
+
+        this.top = this.top.prev;
+        this.size--;
+
+        return result;
+    }
+
+    private void ensureNonEmpty() {
+        if (this.top == null) {
+            throw new IllegalStateException("Empty stack");
+        }
+    }
+
+    public int peak() {
+        ensureNonEmpty();
+        return this.top.element;
+    }
+
+    public void forEach(Consumer<Integer> consumer) {
+        Node current = this.top;
+
+        while (current != null) {
+            consumer.accept(current.element);
+            current = current.prev;
+        }
+    }
+}

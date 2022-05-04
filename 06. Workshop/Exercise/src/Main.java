d1b597f0c438544bcfcf69444738957a8b33f534
yavorhr <76119513+yavorhr@users.noreply.github.com>
2022-05-04 16:33:32 +0300

    LinkedList custom implementation

---
+++ 06. Workshop/Exercise/src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList linkedList = new LinkedList();
+
+        //addFirst()
+        for (int i = 0; i < 4; i++) {
+            linkedList.addFirst(i + 1);
+        }
+
+        /*
+        *** get()
+        System.out.println(linkedList.get(3));
+
+        *** forEach()
+        linkedList.forEach(System.out::println);
+
+        *** removeFirst()
+        while(!linkedList.isEmpty()){
+        System.out.println(linkedList.removeFirst());
+
+        *** toArray()
+        int[] array = linkedList.toArray();
+        for (int num : array){
+            System.out.println(num);
+        }
+
+        //addLast()
+        for (int i = 5; i < 9 ; i++) {
+            linkedList.addLast(i);
+        }
+        linkedList.forEach(System.out::println);
+
+        *** removeLast()
+        linkedList.removeLast();
+        linkedList.removeLast();
+        linkedList.removeLast();
+        linkedList.removeLast();
+        */
+
+
+    }
+}
+

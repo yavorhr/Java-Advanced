7eccba373cb35689b03e26657fd375583f9b4a27
wolfie <yavor.hristozov@gmail.com>
2023-12-12 17:18:04 +0200

    refact : 05. Printer Queue

--- 01.  Stacks and Queues/Lab/04. Matching Brackets/Main.java
+++ 01.  Stacks and Queues/Lab/04. Matching Brackets/Main.java
@@ -5,63 +5,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
         ArrayDeque<Integer> stack = new ArrayDeque<>();
-        String expression = scanner.nextLine();
 
-        for (int i = 0; i < expression.length(); i++) {
-            char currChar = expression.charAt(i);
-            if (currChar == '(') {
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+
+            if (currentChar == '(') {
                 stack.push(i);
-            } else if (currChar == ')') {
-                int openingIndex = stack.pop();
-                int closingIndex = i;
-                System.out.println(expression.substring(openingIndex,closingIndex+1));
+            } else if (currentChar == ')') {
+                int openIndex = stack.pop();
+                int closingIndex = i + 1;
+
+                printSubstring(input, openIndex, closingIndex);
             }
         }
+
+    }
+
+    private static void printSubstring(String input, int openIndex, int closingIndex) {
+        System.out.println(input.substring(openIndex, closingIndex));
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
--- 01.  Stacks and Queues/Lab/05. Printer Queue/Main.java
+++ 01.  Stacks and Queues/Lab/05. Printer Queue/Main.java
@@ -5,64 +5,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
-        String expression = scanner.nextLine();
+        ArrayDeque<String> printerQueue = new ArrayDeque<>();
 
-        for (int i = 0; i < expression.length(); i++) {
-            char currChar = expression.charAt(i);
-            if (currChar == '(') {
-                stack.push(i);
-            } else if (currChar == ')') {
-                int openingIndex = stack.pop();
-                int closingIndex = i;
-                System.out.println(expression.substring(openingIndex,closingIndex+1));
+        String command = scanner.nextLine();
+        while (!"print".equals(command)) {
+            if (command.equals("cancel")) {
+                if (!printerQueue.isEmpty()) {
+                    System.out.printf("Canceled %s\n", printerQueue.poll());
+                } else {
+                    System.out.println("Printer is on standby");
+                }
+            } else {
+                printerQueue.offer(command);
             }
+
+            command = scanner.nextLine();
         }
 
+        printCommands(printerQueue);
+    }
+
+    private static void printCommands(ArrayDeque<String> printerQueue) {
+        while (!printerQueue.isEmpty()){
+            System.out.println(printerQueue.poll());
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 

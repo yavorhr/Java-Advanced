6d54fbfcfeb21ab2fc2736de812b3950472f3853
wolfie <yavor.hristozov@gmail.com>
2023-12-12 16:27:24 +0200

    refact : 02. Simple Calculator
    Extract method for calculating the result

--- 01.  Stacks and Queues/Lab/02. Simple Calculator/Main.java
+++ 01.  Stacks and Queues/Lab/02. Simple Calculator/Main.java
@@ -1,74 +1,43 @@
 import java.util.*;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] input = scanner.nextLine().split("\\s+");
         ArrayDeque<String> stack = new ArrayDeque<>();
 
-        for (int i = input.length - 1; i >= 0; i--) {
-            stack.push(input[i]);
-        }
+        String[] input = scanner.nextLine().split(" ");
+        Collections.reverse(Arrays.asList(input));
+        Arrays.stream(input).forEach(stack::push);
 
         while (stack.size() > 1) {
-            int first = Integer.parseInt(stack.pop());
-            String opp = stack.pop();
-            int second = Integer.parseInt(stack.pop());
+            int firstNumber = Integer.parseInt(stack.pop());
+            String operator = stack.pop();
+            int secondNumber = Integer.parseInt(stack.pop());
 
-            switch (opp) {
-                case "+":
-                    stack.push(String.valueOf(first + second));
-                    break;
-                case "-":
-                    stack.push(String.valueOf(first - second));
-                    break;
+            switch (operator) {
+                case "+" -> calcResult(stack, firstNumber, secondNumber, "+");
+                case "-" -> calcResult(stack, firstNumber, secondNumber, "-");
             }
         }
 
         System.out.println(stack.peek());
     }
+
+    private static void calcResult(ArrayDeque<String> stack, int firstNumber, int secondNumber, String operator) {
+        switch (operator) {
+            case "+" -> {
+                int result = firstNumber + secondNumber;
+                stack.push(String.valueOf(result));
+            }
+            case "-" -> {
+                int result = firstNumber - secondNumber;
+                stack.push(String.valueOf(result));
+            }
+        }
+
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 

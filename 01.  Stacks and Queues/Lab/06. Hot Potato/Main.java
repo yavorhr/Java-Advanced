0c57082e9b96c067d1e696639f187310abbd4760
wolfie <yavor.hristozov@gmail.com>
2023-12-13 16:57:49 +0200

    refact : 06. Hot Potato
    Re-write the program by extracting simple methods from Main method

--- 01.  Stacks and Queues/Lab/06. Hot Potato/Main.java
+++ 01.  Stacks and Queues/Lab/06. Hot Potato/Main.java
@@ -1,69 +1,34 @@
 import java.util.*;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] names = scanner.nextLine().split("\\s+");
+        String[] names = scanner.nextLine().split(" ");
         int n = Integer.parseInt(scanner.nextLine());
 
-        ArrayDeque<String> children = new ArrayDeque();
+        ArrayDeque<String> children = new ArrayDeque<>();
         Collections.addAll(children, names);
 
         while (children.size() > 1) {
             for (int i = 1; i < n; i++) {
-                String currentChild = children.poll();
-                children.offer(currentChild);
+                passingPotato(children);
             }
-
-            String child = children.poll();
-            System.out.println("Removed " + child);
+            String removedChild = children.poll();
+            printResult(removedChild, "Removed %s\n");
         }
-        String lastChild = children.poll();
-        System.out.println("Last is " + lastChild);
+        printResult(children.peek(), "Last is %s\n");
+    }
+
+    private static void printResult(String removedChild, String s) {
+        System.out.printf(s, removedChild);
+    }
+
+    private static void passingPotato(ArrayDeque<String> children) {
+        String currentChild = children.poll();
+        children.offer(currentChild);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 

b5cb427595b45ea8eb351aec0bd3eea56a6e88ee
wolfie <yavor.hristozov@gmail.com>
2023-12-17 14:50:54 +0200

    refact : 02. Positions of
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce Main code
    - make the program more readable
    
    The main functionality remains the same

--- 02.  Multidimensional Arrays/Lab/02. Positions Of/Main.java
+++ 02.  Multidimensional Arrays/Lab/02. Positions Of/Main.java
@@ -1,44 +1,50 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static long[] memoryForFib;
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int rows = scanner.nextInt();
-        int cols = scanner.nextInt();
-        scanner.nextLine();
+        int[][] matrix = readMatrix(scanner);
+        List<int[]> listWithArrays = new ArrayList<>();
 
-        int[][] matrix = new int[rows][cols];
+        fillMatrix(matrix, scanner);
 
-        for (int i = 0; i < rows; i++) {
-            int[] arr = Arrays.stream(scanner.nextLine()
-                    .split("\\s+"))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-            matrix[i] = arr;
-        }
+        int number = Integer.parseInt(scanner.nextLine());
 
-        int n = Integer.parseInt(scanner.nextLine());
-        boolean isFound = false;
         for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix[row].length; col++) {
+            int[] currentArray = matrix[row];
+            for (int col = 0; col < currentArray.length; col++) {
                 int currentNumber = matrix[row][col];
-                if (currentNumber == n) {
-                    System.out.println(row + " " + col);
-                    isFound = true;
+
+                if (currentNumber == number) {
+                    listWithArrays.add(new int[]{row, col});
                 }
             }
         }
 
-        if (!isFound){
+        if (listWithArrays.isEmpty()) {
             System.out.println("not found");
+        } else {
+            listWithArrays.forEach(arr -> System.out.println(arr[0] + " " + arr[1]));
         }
+    }
 
+    private static int[][] readMatrix(Scanner scanner) {
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+        scanner.nextLine();
+
+        return new int[rows][cols];
+    }
+
+    private static void fillMatrix(int[][] matrix, Scanner scanner) {
+        for (int row = 0; row < matrix.length; row++) {
+            int[] arr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::valueOf).toArray();
+            matrix[row] = arr;
+        }
     }
 }
-
-
 

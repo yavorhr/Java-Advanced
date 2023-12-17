15fb462571cbc2072fa4f0825400f95c20678a4f
wolfie <yavor.hristozov@gmail.com>
2023-12-17 15:57:31 +0200

    refact : 03. Intersection of Two Matrices
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce Main code
    - make the program more readable
    
    The main functionality remains the same

--- 02.  Multidimensional Arrays/Lab/03. Intersection of Two Matrices/Main.java
+++ 02.  Multidimensional Arrays/Lab/03. Intersection of Two Matrices/Main.java
@@ -1,51 +1,59 @@
+
 import java.util.Scanner;
 
 public class Main {
-    public static long[] memoryForFib;
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int rows = Integer.parseInt(scanner.nextLine());
-        int columns = Integer.parseInt(scanner.nextLine());
+        int cols = Integer.parseInt(scanner.nextLine());
 
-        char[][] firstMatrix = readMatrix(scanner, rows, columns);
-        char[][] secondMatrix = readMatrix(scanner, rows, columns);
+        char[][] firstMatrix = fillMatrix(scanner, rows, cols);
+        char[][] secondMatrix = fillMatrix(scanner, rows, cols);
+        char[][] outputMatrix = getIntersectedMatrix(firstMatrix, secondMatrix, rows, cols);
 
-        char[][] outputMatrix = new char[rows][columns];
+        printOutputMatrix(outputMatrix, rows, cols);
+    }
+
+    private static void printOutputMatrix(char[][] outputMatrix, int rows, int cols) {
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (col == cols - 1) {
+                    System.out.print(outputMatrix[row][col]);
+                } else {
+                    System.out.print(outputMatrix[row][col] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static char[][] fillMatrix(Scanner scanner, int rows, int cols) {
+        char[][] matrix = new char[rows][cols];
 
         for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < columns; col++) {
+            String[] input = scanner.nextLine().split(" ");
+
+            for (int col = 0; col < input.length; col++) {
+                char currentSymbol = input[col].charAt(0);
+                matrix[row][col] = currentSymbol;
+            }
+        }
+        return matrix;
+    }
+
+    public static char[][] getIntersectedMatrix(char[][] firstMatrix, char[][] secondMatrix, int rows, int cols) {
+        char[][] outputMatrix = new char[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
                 outputMatrix[row][col] =
                         firstMatrix[row][col] == secondMatrix[row][col]
                                 ? firstMatrix[row][col]
                                 : '*';
             }
         }
-
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < columns; col++) {
-                char symbol = outputMatrix[row][col];
-                System.out.print(symbol + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static char[][] readMatrix(Scanner scanner, int rows, int cols) {
-        char[][] matrix = new char[rows][cols];
-
-        for (int row = 0; row < rows; row++) {
-            String[] symbols = scanner.nextLine().split("\\s+");
-            for (int col = 0; col < cols; col++) {
-                matrix[row][col] = symbols[col].charAt(0);
-            }
-        }
-        return matrix;
+        return outputMatrix;
     }
 }
-
-
-
-
 

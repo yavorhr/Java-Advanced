e21af52fdbcf06b1c593b60eb54b968576329f65
wolfie <yavorhristozov@gmail.com>
2023-12-18 17:03:25 +0200

    refact : 05. Maximum Sum of 2x2 Submatrix
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce Main code
    - make the program more readable
    
    The main functionality remains the same

--- 02.  Multidimensional Arrays/Lab/05. Maximum Sum of 2x2 Submatrix/Main.java
+++ 02.  Multidimensional Arrays/Lab/05. Maximum Sum of 2x2 Submatrix/Main.java
@@ -2,32 +2,28 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] dimensions = Arrays.stream(scanner.nextLine().split(", "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        int rows = dimensions[0];
-        int cols = dimensions[1];
+        int[] tokens = readArrayFromConsole(scanner);
+        int rows = tokens[0];
+        int cols = tokens[1];
 
-        int matrix[][] = new int[rows][cols];
+        int[][] matrix = fillMatrix(rows, cols, scanner);
+
         int[][] bestMatrix = new int[2][2];
+        int maxSum = Integer.MIN_VALUE;
 
-        for (int row = 0; row < rows; row++) {
-            matrix[row] = Arrays.stream(scanner.nextLine().split(", "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-        }
-
-        int maxValue = Integer.MIN_VALUE;
         for (int row = 0; row < rows - 1; row++) {
             for (int col = 0; col < cols - 1; col++) {
-                int currentSum = matrix[row][col] + matrix[row][col + 1]
-                        + matrix[row + 1][col] + matrix[row + 1][col + 1];
-                if (currentSum > maxValue) {
-                    maxValue = currentSum;
+                int currentSum = matrix[row][col] +
+                        matrix[row][col + 1] +
+                        matrix[row + 1][col] +
+                        matrix[row + 1][col + 1];
+
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+
                     bestMatrix[0][0] = matrix[row][col];
                     bestMatrix[0][1] = matrix[row][col + 1];
                     bestMatrix[1][0] = matrix[row + 1][col];
@@ -36,23 +32,31 @@ public class Main {
             }
         }
 
+        System.out.println(maxSum);
+        printBestMatrix(bestMatrix);
+    }
+
+    private static int[] readArrayFromConsole(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    private static void printBestMatrix(int[][] bestMatrix) {
         for (int row = 0; row < bestMatrix.length; row++) {
-            for (int col = 0; col < bestMatrix[row].length; col++) {
-                System.out.print(bestMatrix[row][col] + " ");
+            int[] arr = bestMatrix[row];
+            for (int col = 0; col < arr.length; col++) {
+                System.out.print(arr[col] + " ");
             }
             System.out.println();
         }
-        System.out.println(maxValue);
+    }
+
+    public static int[][] fillMatrix(int rows, int cols, Scanner scanner) {
+        int[][] matrix = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            int[] arr = readArrayFromConsole(scanner);
+            matrix[i] = arr;
+        }
+        return matrix;
     }
 }
-
-
-
-
-
-
-
-
-
-
-

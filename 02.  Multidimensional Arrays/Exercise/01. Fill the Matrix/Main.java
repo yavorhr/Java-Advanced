6e48589fc633fa24b4ea08c12850d8c128af0fb5
wolfie <yavorhristozov@gmail.com>
2023-12-19 16:31:21 +0200

    refact : 1.	Fill the Matrix
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce Main code
    - make the program more readable
    
    The main functionality remains the same

--- 02.  Multidimensional Arrays/Exercise/01. Fill the Matrix/Main.java
+++ 02.  Multidimensional Arrays/Exercise/01. Fill the Matrix/Main.java
@@ -1,53 +1,27 @@
+
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] input = scanner.nextLine().split(", ");
-        int size = Integer.parseInt(input[0]);
-        String pattern = input[1];
+        String[] tokens = scanner.nextLine().split(", ");
+        int size = Integer.parseInt(tokens[0]);
+        String pattern = tokens[1];
 
         int[][] matrix = new int[size][size];
-
-        int startN = 1;
+        int incrementN = 1;
 
         if (pattern.equals("A")) {
-            fillPatternA(size, matrix, startN);
+            matrix = fillMatrixPatternA(size, incrementN);
         } else if (pattern.equals("B")) {
-            fillPatternB(size, matrix, startN);
+            matrix = fillMatrixPatternB(size, incrementN);
         }
 
-        printMatrix(matrix,size,size);
+        printMatrix(matrix);
     }
 
-    private static void fillPatternB(int size, int[][] matrix, int startN) {
-
-        for (int col = 0; col < size; col++) {
-            //Четна -> от 0 до SIZE
-            if (col % 2 == 0) {
-                for (int row = 0; row < size; row++) {
-                    matrix[row][col] = startN++;
-                }
-            } else {
-                //Нечетна -> от SIZE-1 до 0
-                for (int row = size - 1; row >= 0; row--) {
-                    matrix[row][col] = startN++;
-                }
-            }
-        }
-    }
-
-    private static void fillPatternA(int size, int[][] matrix, int startN) {
-        for (int col = 0; col < size; col++) {
-            for (int row = 0; row < size; row++) {
-                matrix[row][col] = startN++;
-            }
-        }
-    }
-
-    private static void printMatrix(int[][] matrix,int rows,int cols) {
+    private static void printMatrix(int[][] matrix) {
         for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix[row].length; col++) {
                 System.out.print(matrix[row][col] + " ");
@@ -55,5 +29,32 @@ public class Main {
             System.out.println();
         }
     }
-}
 
+    private static int[][] fillMatrixPatternA(int size, int incrementN) {
+        int[][] matrix = new int[size][size];
+
+        for (int col = 0; col < size; col++) {
+            for (int row = 0; row < size; row++) {
+                matrix[row][col] = incrementN++;
+            }
+        }
+        return matrix;
+    }
+
+    private static int[][] fillMatrixPatternB(int size, int incrementN) {
+        int[][] matrix = new int[size][size];
+
+        for (int col = 0; col < size; col++) {
+            if (col % 2 == 0) {
+                for (int row = 0; row < size; row++) {
+                    matrix[row][col] = incrementN++;
+                }
+            } else {
+                for (int row = size - 1; row >= 0; row--) {
+                    matrix[row][col] = incrementN++;
+                }
+            }
+        }
+        return matrix;
+    }
+}

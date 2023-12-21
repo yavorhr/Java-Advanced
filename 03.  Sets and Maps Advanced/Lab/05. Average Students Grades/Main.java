3c8d8ab4fc61458a7eb9d611ee299d642d18a3e1
wolfie <yavorhristozov@gmail.com>
2023-12-21 21:08:46 +0200

    refact : 05. Average Student Grades
    minor fixes

--- 03.  Sets and Maps Advanced/Lab/05. Average Students Grades/Main.java
+++ 03.  Sets and Maps Advanced/Lab/05. Average Students Grades/Main.java
@@ -2,30 +2,31 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         Map<String, List<Double>> grades = new TreeMap<>();
 
         int n = Integer.parseInt(scanner.nextLine());
+
         for (int i = 0; i < n; i++) {
             String[] input = scanner.nextLine().split("\\s+");
             String studentName = input[0];
             double studentGrade = Double.parseDouble(input[1]);
 
             grades.putIfAbsent(studentName, new ArrayList<>());
-            if (grades.containsKey(studentName)) {
-                List<Double> currentGrades = grades.get(studentName);
-                currentGrades.add(studentGrade);
-                grades.put(studentName, currentGrades);
-            }
+
+            List<Double> currentGrades = grades.get(studentName);
+            currentGrades.add(studentGrade);
+            grades.put(studentName, currentGrades);
         }
 
         for (Map.Entry<String, List<Double>> entry : grades.entrySet()) {
-            System.out.println(String.format("%s -> %s (avg: %.2f)",
+            System.out.printf("%s -> %s (avg: %.2f)%n",
                     entry.getKey(),
-                    gradesAsString (entry.getValue()),
-                    avgGrade(entry.getValue())));
+                    gradesAsString(entry.getValue()),
+                    avgGrade(entry.getValue()));
         }
     }
 
@@ -37,13 +38,8 @@ public class Main {
     }
 
     private static String gradesAsString(List<Double> grades) {
-        return  grades.stream()
-                .map(grade -> String.format("%.2f",grade))
+        return grades.stream()
+                .map(grade -> String.format("%.2f", grade))
                 .collect(Collectors.joining(" "));
     }
 }
-
-
-
-
-

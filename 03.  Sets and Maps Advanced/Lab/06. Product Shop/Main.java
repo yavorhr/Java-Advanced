b8dc714437396dfd38a18f737bb28848d07071c7
wolfie <yavorhristozov@gmail.com>
2023-12-21 21:23:37 +0200

    refact : 06. Product Shop
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce Main code
    - make the program more readable
    
    The main functionality remains the same

--- 03.  Sets and Maps Advanced/Lab/06. Product Shop/Main.java
+++ 03.  Sets and Maps Advanced/Lab/06. Product Shop/Main.java
@@ -1,43 +1,36 @@
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Map<String, Map<String, Double>> costs = new TreeMap<>();
-        String line = scanner.nextLine();
-        while (!line.equalsIgnoreCase("Revision")) {
-            String[] tokens = line.split(", ");
+        Map<String, Map<String, Double>> registry = new TreeMap<>();
 
+        String input = scanner.nextLine();
+        while (!"Revision".equals(input)) {
+            String[] tokens = input.split(", ");
             String shop = tokens[0];
             String product = tokens[1];
             Double price = Double.parseDouble(tokens[2]);
 
-            costs.putIfAbsent(shop, new LinkedHashMap<>());
-            costs.get(shop).put(product, price);
+            addProductsToShop(registry, shop, product, price);
 
-            line = scanner.nextLine();
+            input = scanner.nextLine();
         }
 
-        costs.entrySet()
-                .stream()
-                .forEach(entry -> {
-                    System.out.println(entry.getKey() + "->");
-                    entry.getValue()
-                            .entrySet()
-                            .stream()
-                            .forEach(innerEntry -> {
-                                System.out.printf("Product: %s, Price: %.1f%n",
-                                        innerEntry.getKey(),
-                                        innerEntry.getValue());
-                            });
-                });
+        registry.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey());
+            entry.getValue().entrySet().forEach(innerEntry -> {
+                System.out.printf("Product: %s, Price: %.2f\n", innerEntry.getKey(), innerEntry.getValue());
+            });
+        });
+    }
+
+    private static void addProductsToShop(Map<String, Map<String, Double>> registry, String market, String product, Double price) {
+        registry.putIfAbsent(market, new HashMap<>());
+        Map<String, Double> products = registry.get(market);
+        products.put(product, price);
+        registry.put(market, products);
     }
 }
-
-
-
-
-
-
-

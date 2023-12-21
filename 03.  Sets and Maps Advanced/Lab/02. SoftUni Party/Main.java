fe54b1e1011316da5fc3582852a851f73db4fcf1
wolfie <yavorhristozov@gmail.com>
2023-12-21 15:18:24 +0200

    refact : 02. SoftUni Party
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce Main code
    - make the program more readable
    
    The main functionality remains the same

--- 03.  Sets and Maps Advanced/Lab/02. SoftUni Party/Main.java
+++ 03.  Sets and Maps Advanced/Lab/02. SoftUni Party/Main.java
@@ -1,33 +1,54 @@
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Set<String> vips = new LinkedHashSet<>();
-        Set<String> regular = new LinkedHashSet<>();
+        Set<String> regular = new HashSet<>();
+        Set<String> vip = new HashSet<>();
 
         String input = scanner.nextLine();
         while (!"PARTY".equals(input)) {
-            if (Character.isDigit(input.charAt(0))) {
-                vips.add(input);
-            } else {
-                regular.add(input);
-            }
+
+            addGuestToLists(regular, vip, input);
+
             input = scanner.nextLine();
         }
 
-        Set<String> allGuests = new TreeSet<>(vips);
-        allGuests.addAll(regular);
+        Set<String> allGuests = new TreeSet<>(regular);
+        allGuests.addAll(vip);
 
-        while (!"END".equals(input)) {
-            allGuests.remove(input);
+        String guestNameToVisit = scanner.nextLine();
+        while (!"END".equals(guestNameToVisit)) {
 
-            input = scanner.nextLine();
+            allGuests.remove(guestNameToVisit);
 
+            guestNameToVisit = scanner.nextLine();
         }
 
+        printMissingGuests(allGuests);
+    }
+
+    private static void printMissingGuests(Set<String> allGuests) {
         System.out.println(allGuests.size());
-        System.out.println(String.join(System.lineSeparator(), allGuests));
+        allGuests.forEach(System.out::println);
+    }
+
+    private static void addGuestToLists(Set<String> regular, Set<String> vip, String input) {
+        if (Character.isDigit(input.charAt(0))) {
+            vip.add(input);
+        } else {
+            regular.add(input);
+        }
     }
 }
+
+
+
+
+
+
+
+
+

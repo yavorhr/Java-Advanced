dd6da01fb45a5e1215bdbdf3c3db8de56dc3e733
wolfie <yavorhristozov@gmail.com>
2023-12-23 15:51:48 +0200

    refact : 07. Fix Emails
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce Main code
    - make the program more readable
    
    The main functionality remains the same

--- 03.  Sets and Maps Advanced/Exercise/07. Fix Emails/Main.java
+++ 03.  Sets and Maps Advanced/Exercise/07. Fix Emails/Main.java
@@ -4,40 +4,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        LinkedHashMap<String, String> emails = new LinkedHashMap<>();
+        Map<String, String> allEmails = new LinkedHashMap<>();
 
-        String input = scanner.nextLine();
-        while (!"stop".equals(input)) {
-            String userName = input;
-            String emailAddress = scanner.nextLine();
+        String name = scanner.nextLine();
+        while (!"stop".equals(name)) {
+            String email = scanner.nextLine();
 
-            int dotIndex = emailAddress.lastIndexOf('.');
-            String domain = emailAddress.substring(dotIndex + 1);
+            addEmail(allEmails, name, email);
 
-            if (domain.equals("bg")) {
-                emails.put(userName, emailAddress);
-            }
-            input = scanner.nextLine();
+            name = scanner.nextLine();
         }
-        emails.entrySet().forEach(entry -> {
-            System.out.println(String.format("%s -> %s",entry.getKey(),entry.getValue()));
-        });
+        printResult(allEmails);
+    }
+
+    private static void printResult(Map<String, String> allEmails) {
+        allEmails
+                .entrySet()
+                .forEach(entry -> System.out.printf("%s - %s\n",
+                        entry.getKey(),
+                        entry.getValue()));
+    }
+
+    private static void addEmail(Map<String, String> allEmails, String name, String email) {
+        String domain = email.substring(email.indexOf('.')+1);
+        if (domain.equals("bg")) {
+            allEmails.putIfAbsent(name, email);
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

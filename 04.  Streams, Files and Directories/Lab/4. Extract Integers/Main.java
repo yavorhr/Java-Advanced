03b80131913dbf8b3eee9717fef96f650bd5e1d4
wolfie <yavorhristozov@gmail.com>
2023-12-24 16:18:11 +0200

    refact : 04 Extract Integers
    Add input.txt and output.txt
    Add method for writing integers to output file

--- 04.  Streams, Files and Directories/Lab/4. Extract Integers/Main.java
+++ 04.  Streams, Files and Directories/Lab/4. Extract Integers/Main.java
@@ -1,27 +1,29 @@
-
 import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String path = "C:\\input.txt";
+        String path = "C:\\Users\\YavorHr\\Desktop\\Demo\\input.txt";
         Scanner scanner = new Scanner(new FileInputStream(path));
 
         PrintWriter writer = new PrintWriter("integers-out.txt");
 
         while (scanner.hasNext()) {
-            if (scanner.hasNextInt()) {
-                int number = scanner.nextInt();
-                writer.println(number);
-            }
+
+            writeIntToFile(scanner, writer);
+
             scanner.next();
         }
 
         writer.close();
     }
+
+    private static void writeIntToFile(Scanner scanner, PrintWriter writer) {
+        if (scanner.hasNextInt()) {
+            int number = scanner.nextInt();
+            writer.println(number);
+        }
+    }
 }
-
-
-
 

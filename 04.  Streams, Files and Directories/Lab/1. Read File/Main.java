776cf27406400948b3a0d4a0aafda9bdfac0cdce
wolfie <yavorhristozov@gmail.com>
2023-12-24 15:34:07 +0200

    refact : 01. Read File
    Add input.txt resource
    Add method for printing ASCII to Binary numbers

--- 04.  Streams, Files and Directories/Lab/1. Read File/Main.java
+++ 04.  Streams, Files and Directories/Lab/1. Read File/Main.java
@@ -1,40 +1,31 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
 
-        String path = "C:\\input.txt";
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
 
-        try (FileInputStream fileStream = new FileInputStream(path)) {
-            int oneByte = fileStream.read();
-            while (oneByte != -1 ) {
-                System.out.print(Integer.toBinaryString(oneByte) + " ");
-                oneByte = fileStream.read();
-            }
-        }
-        catch (IOException e) {
+        String filePath = "C:\\Users\\YavorHr\\Desktop\\Demo\\src\\input.txt";
+
+        try {
+            FileInputStream inputStream = new FileInputStream(filePath);
+            int oneByte = inputStream.read();
+
+            printASCIItoBinary(inputStream, oneByte);
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private static void printASCIItoBinary(FileInputStream inputStream, int oneByte) throws IOException {
+        while (oneByte != -1) {
+            System.out.print(Integer.toBinaryString(oneByte) + " ");
+            oneByte = inputStream.read();
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

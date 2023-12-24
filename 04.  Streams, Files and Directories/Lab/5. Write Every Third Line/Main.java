f31d46b56ee3b603a64eaa64aeb89419fed61d3e
wolfie <yavorhristozov@gmail.com>
2023-12-24 16:32:34 +0200

    refact :
    Add input and output files
    Add writing method to output file

--- 04.  Streams, Files and Directories/Lab/5. Write Every Third Line/Main.java
+++ 04.  Streams, Files and Directories/Lab/5. Write Every Third Line/Main.java
@@ -1,32 +1,31 @@
-
 import java.io.*;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String path = "C:\\input.txt";
+        String path = "C:\\Users\\YavorHr\\Desktop\\Demo\\input.txt";
         BufferedReader reader = new BufferedReader(new FileReader(path));
+        BufferedWriter writer = new BufferedWriter(new FileWriter("05. WriteEveryThirdLine.txt"));
 
-        Stream<String> lines = reader.lines();
-        List<String> stringLines = lines.collect(Collectors.toList());
-        BufferedWriter writer = new BufferedWriter(new FileWriter("by-three.txt"));
+        Stream<String> inputLines = reader.lines();
+        List<String> linesList = inputLines.collect(Collectors.toList());
 
-        for (int i = 0; i < stringLines.size(); i++) {
-            if ((i + 1) % 3 == 0) {
-                writer.write(stringLines.get(i));
+        WriteEveryThirdLine(writer, linesList);
+
+        writer.close();
+    }
+
+    private static void WriteEveryThirdLine(BufferedWriter writer, List<String> linesList) throws IOException {
+        for (int i = 0; i < linesList.size(); i+=3) {
+            if (i % 3 == 0) {
+                writer.write(linesList.get(i));
                 writer.newLine();
             }
         }
-        writer.close();
     }
+
 }
-
-
-
-
-
 

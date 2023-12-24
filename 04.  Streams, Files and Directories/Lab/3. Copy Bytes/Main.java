5428571b857350a9fed051c9881e67d797f7c870
wolfie <yavorhristozov@gmail.com>
2023-12-24 16:02:32 +0200

    refact : 03. Copy Bytes
    Add input.txt and output.txt resources
    Add comments to code

--- 04.  Streams, Files and Directories/Lab/3. Copy Bytes/Main.java
+++ 04.  Streams, Files and Directories/Lab/3. Copy Bytes/Main.java
@@ -1,28 +1,25 @@
-
 import java.io.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
 
-        String path = "C:\\input.txt";
-        FileInputStream inputStream = new FileInputStream(path);
-
-        String table = ",.!?";
-
+        String filePath = "C:\\Users\\YavorHr\\Desktop\\Demo\\src\\input.txt";
+        FileInputStream inputStream = new FileInputStream(filePath);
         FileOutputStream fileOutputStream = new FileOutputStream("output.txt");
 
         int nextByte = inputStream.read();
         while (nextByte != -1) {
             if (nextByte == ' ' || nextByte == '\n') {
+                //add ASCII for ' ' or ||
                 fileOutputStream.write(nextByte);
             } else {
+                //add binary code for rest
                 fileOutputStream.write(String.valueOf(nextByte).getBytes());
             }
             nextByte = inputStream.read();
         }
+
         fileOutputStream.close();
+        inputStream.close();
     }
 }
-
-

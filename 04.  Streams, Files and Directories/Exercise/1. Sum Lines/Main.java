2b1930f63a17fc33647abbbe6ac94a339da944bb
wolfie <yavorhristozov@gmail.com>
2023-12-27 11:19:24 +0200

    refact : 01. Sum Lines
    Add input resource
    Add method for printing ASCC sum per string line

--- 04.  Streams, Files and Directories/Exercise/1. Sum Lines/Main.java
+++ 04.  Streams, Files and Directories/Exercise/1. Sum Lines/Main.java
@@ -5,27 +5,21 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Path path = Path.of("input.txt");
 
-        Path pathfile = Path.of("C:\\Users\\YavorHr\\Documents\\1. SoftUni\\Advanced" +
-                "\\4. Streams, Files And Directories - Lab\\УПР\\04. Java-Advanced-Streams-Files-and-Directories-Resources" +
-                "\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt");
+        List<String> lines = Files.readAllLines(path);
 
-        List<String> lines = Files.readAllLines(pathfile);
-        lines.stream().map(String::toCharArray).forEach(charArray -> {
+        printASCIIsumPerLine(lines);
+    }
+
+    private static void printASCIIsumPerLine(List<String> lines) {
+        lines.stream().map(String::toCharArray).forEach(charArr -> {
             int sum = 0;
-            for (char symbol : charArray) {
+            for (char symbol : charArr) {
                 sum += symbol;
             }
             System.out.println(sum);
         });
     }
 }
-
-
-
-
-
-
-
-
 

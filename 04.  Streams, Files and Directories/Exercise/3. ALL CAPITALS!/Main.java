95d8bc9084bd458c79f0fd66fe4bdf1f127fed89
wolfie <yavorhristozov@gmail.com>
2023-12-27 11:57:42 +0200

    refact : 03. ALL CAPITALS!
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce Main code
    - make the program more readable
    
    The main functionality remains the same

--- 04.  Streams, Files and Directories/Exercise/3. ALL CAPITALS!/Main.java
+++ 04.  Streams, Files and Directories/Exercise/3. ALL CAPITALS!/Main.java
@@ -5,27 +5,20 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Path path = Path.of("input.txt");
+        List<String> lines = Files.readAllLines(path);
 
-        Path pathfile = Path.of("C:\\Users\\YavorHr\\Documents\\1. SoftUni\\Advanced" +
-                "\\4. Streams, Files And Directories - Lab\\УПР\\04. Java-Advanced-Streams-Files-and-Directories-Resources" +
-                "\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt");
+        writeCapitalizedLines(lines);
+    }
 
+    private static void writeCapitalizedLines(List<String> lines) throws FileNotFoundException {
+        PrintWriter printWriter = new PrintWriter("output.txt");
 
-        List<String> lines = Files.readAllLines(pathfile);
-        PrintWriter writer = new PrintWriter("output.txt");
-        lines.forEach(line -> {
-            writer.println(line.toUpperCase());
-        });
-        writer.close();
+        for (String line : lines) {
+            printWriter.println( line.toUpperCase());
+        }
+
+        printWriter.close();
     }
 }
-
-
-
-
-
-
-
-
-
 

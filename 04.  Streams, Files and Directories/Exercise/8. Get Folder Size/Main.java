873d08b03c98b7da3037f64c56129baf5df2bf8e
wolfie <yavorhristozov@gmail.com>
2023-12-27 15:21:22 +0200

    refact : 08. Get Folder Size
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce Main code
    - make the program more readable
    
    The main functionality remains the same

--- 04.  Streams, Files and Directories/Exercise/8. Get Folder Size/Main.java
+++ 04.  Streams, Files and Directories/Exercise/8. Get Folder Size/Main.java
@@ -2,34 +2,21 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        String folderPath = "C:\\Users\\YavorHr\\Documents\\1. SoftUni\\Advanced\\4. Streams, Files And Directories - Lab\\УПР\\04. Java-Advanced-Streams-Files-and-Directories-Resources\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\Exercises Resources";
+        String folderPath = "E:\\1. SoftUni\\Code\\Java\\03. Java Advanced\\Advanced\\04. Streams, Files And Directories - Lab\\Exercise\\04. Java-Advanced-Streams-Files-and-Directories-Resources\\04. Java-Advanced-Files-and-Streams-Exercises-Resources";
 
         File folder = new File(folderPath);
+        long totalSize = getLengthInBytes(folder);
 
+        System.out.println(totalSize);
+    }
+
+    private static long getLengthInBytes(File folder) {
         long size = 0;
+
         for (File file : folder.listFiles()) {
             size += file.length();
         }
-
-        System.out.println("Folder size: " + size);
+        return size;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 

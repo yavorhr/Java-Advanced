9941a0f1020c6055c1df3e395b6102336f7628f0
wolfie <yavorhristozov@gmail.com>
2023-12-28 16:10:01 +0200

    refact : 02. Constructors
    Re-write the code in Main by extracting simple methods and rename some variables in order to :
    - reduce Main code
    - make the program more readable
    
    The main functionality remains the same

--- 05.  Defining classes/Lab/02.  Constructors/Main.java
+++ 05.  Defining classes/Lab/02.  Constructors/Main.java
@@ -1,37 +1,43 @@
-package Google;
-
-import java.util.*;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
 
-        List<Car> cars = new LinkedList<>();
-        int n = Integer.parseInt(scanner.nextLine());
-        for (int i = 0; i < n; i++) {
-            String[] tokens = scanner.nextLine().split("\\s+");
-            if (tokens.length == 3) {
-                String model = tokens[0];
-                String brand = tokens[1];
-                int horsePower = Integer.parseInt(tokens[2]);
-                Car car = new Car(model, brand, horsePower);
-                cars.add(car);
-            } else if (tokens.length == 1) {
-                String model = tokens[0];
-                Car car = new Car(model);
-                cars.add(car);
-            }
-        }
-        for (Car car : cars) {
-            System.out.println(car.carInfo());
+        while (n > 0) {
+            Car car = getCar(scanner);
+
+            printCar(car);
+
+            n--;
         }
     }
+
+    private static void printCar(Car car) {
+        System.out.println(car.carInfo());
+    }
+
+    private static Car getCar(Scanner scanner) {
+        String[] tokens = scanner.nextLine().split(" ");
+        String model = tokens[0];
+
+        Car car = null;
+
+        if (tokens.length == 1) {
+            car = new Car(model);
+        } else if (tokens.length == 2) {
+            String brand = tokens[1];
+            car = new Car(model, brand);
+        } else if (tokens.length == 3) {
+            String brand = tokens[1];
+            int hp = Integer.parseInt(tokens[2]);
+            car = new Car(model, brand, hp);
+        }
+
+        return car;
+    }
 }
-
-
-
-
-
-
-
 

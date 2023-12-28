962e4b3722ebc011cc5280a49f0963352d512db1
wolfie <yavorhristozov@gmail.com>
2023-12-28 16:09:16 +0200

    refact : 02. Constructors
    add constructors overload in Car

--- 05.  Defining classes/Lab/02.  Constructors/Car.java
+++ 05.  Defining classes/Lab/02.  Constructors/Car.java
@@ -1,25 +1,57 @@
-package Google;
-
 public class Car {
     private String brand;
     private String model;
     private int horsePower;
 
     public Car(String brand, String model, int horsePower) {
-        this.brand = brand;
-        this.model = model;
+        this(brand, model);
         this.horsePower = horsePower;
+    }
+
+    public Car(String brand, String model) {
+        this(brand);
+        this.model = model;
+        this.horsePower = -1;
     }
 
     public Car(String brand) {
         this.brand = brand;
         this.model = "unknown";
         this.horsePower = -1;
-
     }
 
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Car setModel(String model) {
+        this.model = model;
+        return this;
+    }
+
+    public Car setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+        return this;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public int getHorsePower() {
+        return this.horsePower;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("The car is: %s %s - %d HP", getBrand(), getModel(), getHorsePower());
+    }
 
     public String carInfo() {
-        return String.format("The car is: %s %s - %d HP.", this.brand, this.model, this.horsePower);
+        return toString();
     }
 }

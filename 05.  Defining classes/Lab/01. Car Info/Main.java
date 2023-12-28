19ddacf6d471f4655dbbb234df5ba1c3f9f5c772
wolfie <yavorhristozov@gmail.com>
2023-12-28 15:41:34 +0200

    refact : 01. Car Info
    re-write the program according to the requirements

--- 05.  Defining classes/Lab/01. Car Info/Main.java
+++ 05.  Defining classes/Lab/01. Car Info/Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         scanner.nextLine();
@@ -16,7 +16,7 @@ public class Main {
     }
 
     private static void printCar(Car car) {
-        System.out.println(car.carInfo());
+        System.out.printf("The car is: %s %s - %d HP%n", car.getBrand(), car.getModel(), car.getHorsePower());
     }
 
     private static Car getCar(Scanner scanner) {
@@ -25,7 +25,12 @@ public class Main {
         String model = tokens[1];
         int hp = Integer.parseInt(tokens[2]);
 
-        Car car = new Car(brand,model,hp);
+        Car car = new Car();
+
+        car.setBrand(brand);
+        car.setModel(model);
+        car.setHorsePower(hp);
+
         return car;
     }
 }

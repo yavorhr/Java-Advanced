569bb7dec2f595406db3da188c8fdd610d004ea5
wolfie <yavorhristozov@gmail.com>
2023-12-28 15:36:46 +0200

    refact : 01. Car Info
    Re-write the Main by extracting simple methods and rename some variables in order to :
    - reduce Main code
    - make the program more readable
    
    The main functionality remains the same

--- 05.  Defining classes/Lab/01. Car Info/Main.java
+++ 05.  Defining classes/Lab/01. Car Info/Main.java
@@ -4,26 +4,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
 
+        while (n > 0) {
+            Car car = getCar(scanner);
+            printCar(car);
 
-        int n = Integer.parseInt(scanner.nextLine());
-        for (int i = 0; i < n; i++) {
-            String[] tokens = scanner.nextLine().split("\\s+");
-            Car car = new Car();
-            car.setBrand(tokens[0]);
-            car.setModel(tokens[1]);
-            car.setHorsePower(Integer.parseInt(tokens[2]));
-
-            System.out.println(String.format("The car is: %s %s - %d HP.",car.getBrand(),car.getModel(),car.getHorsePower()));
+            n--;
         }
+    }
 
+    private static void printCar(Car car) {
+        System.out.println(car.carInfo());
+    }
 
+    private static Car getCar(Scanner scanner) {
+        String[] tokens = scanner.nextLine().split(" ");
+        String brand = tokens[0];
+        String model = tokens[1];
+        int hp = Integer.parseInt(tokens[2]);
 
+        Car car = new Car(brand,model,hp);
+        return car;
     }
 }
-
-
-
-
-
 

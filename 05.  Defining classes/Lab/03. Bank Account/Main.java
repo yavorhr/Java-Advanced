91908cd20cdb81d4a4c7d346ce8e9bf8904c3891
wolfie <yavorhristozov@gmail.com>
2023-12-28 19:10:17 +0200

    refact : 03. Bank Account - Main
    Re-write the Main class by extracting simple methods and rename some variables in order to :
    - reduce Main code
    - make the program more readable
    
    The main functionality remains the same

--- 05.  Defining classes/Lab/03. Bank Account/Main.java
+++ 05.  Defining classes/Lab/03. Bank Account/Main.java
@@ -1,6 +1,5 @@
-package Google;
-
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,42 +9,64 @@ public class Main {
 
         String input = scanner.nextLine();
         while (!"End".equals(input)) {
-            String[] tokens = input.split("\\s+");
+            String[] tokens = input.split(" ");
             String command = tokens[0];
-            if (command.equals("Create")) {
-                BankAccount bankAccount = new BankAccount();
-                bankAccounts.put(bankAccount.getId(), bankAccount);
-            } else if (command.equals("Deposit")) {
-                int id = Integer.parseInt(tokens[1]);
-                if (bankAccounts.containsKey(id)) {
-                    BankAccount currentBankAccount = bankAccounts.get(id);
-                    currentBankAccount.deposit(Double.parseDouble(tokens[2]));
-                } else {
-                    System.out.println("Account does not exist");
+
+            switch (command) {
+                case "Create" -> {
+                    BankAccount bankAccount = createBankAccount(bankAccounts);
+                    System.out.printf("Account ID%d created\n", bankAccount.getId());
                 }
-            } else if (command.equals("SetInterest")) {
-                double newInterest = Double.parseDouble(tokens[1]);
-                BankAccount.setInterestRate(newInterest);
-            } else {
-                int id = Integer.parseInt(tokens[1]);
-                if (bankAccounts.containsKey(id)) {
-                    BankAccount bankAccount = bankAccounts.get(id);
-                    System.out.printf("%.2f%n", bankAccount.getInterest(Integer.parseInt(tokens[2])));
-                } else {
-                    System.out.println("Account does not exist");
+                case "Deposit" -> {
+                    int id = Integer.parseInt(tokens[1]);
+                    double balance = Double.parseDouble(tokens[2]);
+
+                    if (doesAccountExist(bankAccounts, id)) {
+                        BankAccount bankAccount = bankAccounts.get(id);
+                        bankAccount.setBalance(balance);
+                        bankAccounts.put(id, bankAccount);
+                        System.out.printf("Deposited %.2f to ID%d\n", balance, id);
+                    } else {
+                        System.out.println("Account does not exist");
+                    }
+                }
+                case "GetInterest" -> {
+                    int id = Integer.parseInt(tokens[1]);
+                    int years = Integer.parseInt(tokens[2]);
+                    if (doesAccountExist(bankAccounts, id)) {
+                        double calculatedInterest = bankAccounts.get(id).getInterest(years);
+                        System.out.printf("%.2f\n", calculatedInterest);
+                    } else {
+                        System.out.println("Account does not exist");
+                    }
+                }
+                case "SetInterest" -> {
+                    double balance = Double.parseDouble(tokens[1]);
+                    BankAccount.setInterestRate(balance);
                 }
             }
+
             input = scanner.nextLine();
         }
+        System.out.println();
+    }
+
+    private static BankAccount createBankAccount(Map<Integer, BankAccount> bankAccounts) {
+        BankAccount bankAccount = new BankAccount();
+        bankAccounts.put(bankAccount.getId(), bankAccount);
+        return bankAccount;
+    }
+
+    private static boolean doesAccountExist(Map<Integer, BankAccount> bankAccounts, int id) {
+        return bankAccounts.containsKey(id);
+    }
+
+    private static boolean doesAccountExist(BankAccount bankAccount) {
+        return bankAccount != null;
+    }
+
+    private static BankAccount getBankAccount(List<BankAccount> bankAccountsList, int id) {
+        return bankAccountsList.stream().filter(b -> b.getId() == id).findFirst().orElse(null);
     }
 }
-
-
-
-
-
-
-
-
-
 

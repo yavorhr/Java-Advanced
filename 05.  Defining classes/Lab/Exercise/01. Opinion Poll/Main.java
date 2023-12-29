ba0707b2f7b1b6aca4e2f395df559a354a30e615
wolfie <yavorhristozov@gmail.com>
2023-12-29 14:47:37 +0200

    refact : 01. Opinion Poll - Main
    Re-write the Main method by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the class more readable
    
    The main functionality remains the same

--- 05.  Defining classes/Lab/Exercise/01. Opinion Poll/Main.java
+++ 05.  Defining classes/Lab/Exercise/01. Opinion Poll/Main.java
@@ -1,36 +1,40 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    private static final int MAX_AGE = 30;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Person> people = new ArrayList<>();
-        int n = Integer.parseInt(scanner.nextLine());
-        for (int i = 0; i < n; i++) {
-            String[] input = scanner.nextLine().split("\\s+");
-            String name = input[0];
-            int age = Integer.parseInt(input[1]);
+        int n = scanner.nextInt();
+        scanner.nextLine();
 
-            if (age > 30) {
-                Person person = new Person(name, age);
-                people.add(person);
-            }
+        List<Person> peopleList = new ArrayList<>();
+
+        while (n > 0) {
+
+            addPersonToList(scanner, peopleList);
+
+            n--;
         }
 
-        people.sort(Comparator.comparing(p -> p.getName()));
-        for (Person person : people) {
-            System.out.println(person.toString());
-        }
+        printPeopleOver30years(peopleList);
+    }
 
+    private static void printPeopleOver30years(List<Person> peopleList) {
+        peopleList
+                .stream()
+                .filter(p -> p.getAge() > 30)
+                .sorted((p1, p2) -> p1.getName().compareTo(p2.getName()))
+                .forEach(p -> System.out.printf("%s - %d\n", p.getName(), p.getAge()));
+    }
 
+    private static void addPersonToList(Scanner scanner, List<Person> peopleList) {
+        String[] tokens = scanner.nextLine().split(" ");
+        String name = tokens[0];
+        int age = Integer.parseInt(tokens[1]);
+
+        peopleList.add(new Person(name, age));
     }
 }
-
-
-
-
-
 

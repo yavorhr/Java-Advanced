daf48650a7c50454a49b63dc4cddbec64b32ae7e
wolfie <yavorhristozov@gmail.com>
2023-12-30 18:42:23 +0200

    refact : 05. Car Salesman - Engine
    Refactor toString() method
    Remove comments

--- 05.  Defining classes/Lab/Exercise/05. Car Salesman/Engine.java
+++ 05.  Defining classes/Lab/Exercise/05. Car Salesman/Engine.java
@@ -1,57 +1,76 @@
-package Car;
-
 public class Engine {
-        //model, power, displacement and efficiency
-        private String model;
-        private int power;
-        //optional
-        private int displacement;
-        private String efficiency;
-        //1. начин -> model, power
-        //2. начин -> model, power,displacement
-        //3.начин -> model, power, efficiency
-        //4. начин -> model, power, displacement, efficiency
+    private String model;
+    private int power;
+    private int displacement;
+    private String efficiency;
 
-        public Engine (String model, int power) {
-            this.model = model;
-            this.power = power;
-            this.displacement = 0;
-            this.efficiency = "n/a";
-
-        }
-
-        public Engine (String model, int power, int displacement) {
-            this(model, power);
-            this.displacement = displacement;
-            this.efficiency = "n/a";
-        }
-
-        public Engine (String model, int power, String efficiency) {
-            this(model, power);
-            this.efficiency = efficiency;
-            this.displacement = 0;
-        }
-
-        public Engine (String model, int power, int displacement, String efficiency) {
-            this(model, power);
-            this.displacement = displacement;
-            this.efficiency = efficiency;
-        }
-
-        public String getModel() {
-            return this.model;
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("Power: " + this.power + "\n");
-            if(this.displacement == 0) {
-                stringBuilder.append("Displacement: n/a" + "\n");
-            } else {
-                stringBuilder.append("Displacement: " + this.displacement + "\n");
-            }
-            stringBuilder.append("Efficiency: " + this.efficiency);
-            return stringBuilder.toString();
-        }
+    public Engine(String model, int power, int displacement, String efficiency) {
+        this(model, power, displacement);
+        this.efficiency = efficiency;
     }
+
+    public Engine(String model, int power, int displacement) {
+        this(model, power);
+        this.displacement = displacement;
+        this.efficiency = "n/a";
+    }
+
+    public Engine(String model, int power, String efficiency) {
+        this(model, power);
+        this.efficiency = efficiency;
+        this.displacement = 0;
+    }
+
+    public Engine(String model, int power) {
+        this.model = model;
+        this.power = power;
+        this.displacement = 0;
+        this.efficiency = "n/a";
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Engine setModel(String model) {
+        this.model = model;
+        return this;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public Engine setPower(int power) {
+        this.power = power;
+        return this;
+    }
+
+    public int getDisplacement() {
+        return displacement;
+    }
+
+    public Engine setDisplacement(int displacement) {
+        this.displacement = displacement;
+        return this;
+    }
+
+    public String getEfficiency() {
+        return efficiency;
+    }
+
+    public Engine setEfficiency(String efficiency) {
+        this.efficiency = efficiency;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                        Power: %d
+                        Displacement: %s
+                        Efficiency: %s
+                        """
+                , getPower(), this.displacement == 0 ? "n/a" : getDisplacement(), getEfficiency());
+    }
+}

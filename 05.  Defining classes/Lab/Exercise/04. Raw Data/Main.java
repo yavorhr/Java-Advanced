c82601c84045a36f547a5c5e5098481780f43099
wolfie <yavorhristozov@gmail.com>
2023-12-30 15:10:58 +0200

    refact : 04. Raw Data - Main
    Re-write the Main class by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the class more readable
    
    The main functionality remains the same

--- 05.  Defining classes/Lab/Exercise/04. Raw Data/Main.java
+++ 05.  Defining classes/Lab/Exercise/04. Raw Data/Main.java
@@ -1,63 +1,61 @@
-package raw;
-
+import java.sql.Array;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = scanner.nextInt();
+        scanner.nextLine();
 
-        LinkedList<Car> cars = new LinkedList<>();
-        Set<Car> output = new HashSet<>();
+        List<Car> carsList = new LinkedList<>();
 
-        int n = Integer.parseInt(scanner.nextLine());
-        for (int i = 0; i < n; i++) {
-            String[] input = scanner.nextLine().split("\\s+");
-            //ChevroletAstro 200 180 1000 fragile 1.3 1 1.5 2 1.4 2 1.7 4
-            //Model>
-            // <EngineSpeed> <EnginePower>
-            // <CargoWeight> <CargoType>
-            // <Tire1Pressure> <Tire1Age> <Tire2Pressure> <Tire2Age> <Tire3Pressure> <Tire3Age> <Tire4Pressure> <Tire4Age>"
-            String model = input[0];
+        while (n > 0) {
+            String[] parts = scanner.nextLine().split(" ");
+            String carModel = parts[0];
+            int engineSpeed = Integer.parseInt(parts[1]);
+            int enginePower = Integer.parseInt(parts[2]);
+            int cargoWeight = Integer.parseInt(parts[3]);
+            String cargoType = parts[4];
 
-            int engineSpeed = Integer.parseInt(input[1]);
-            int enginePower = Integer.parseInt(input[2]);
+            List<Tire> tires = getTires(parts);
             Engine engine = new Engine(engineSpeed, enginePower);
+            Cargo cargo = new Cargo(cargoType, cargoWeight);
+            Car car = new Car(carModel, engine, cargo, tires);
+            carsList.add(car);
 
-            int cargoWeight = Integer.parseInt(input[3]);
-            String cargoType = input[4];
-            Cargo cargo = new Cargo(cargoWeight, cargoType);
-
-            List<Tire> tires = new ArrayList<>();
-            for (int j = 5; j < 12; j += 2) {
-                double tirePressure = Double.parseDouble(input[j]);
-                int tireAge = Integer.parseInt(input[j + 1]);
-                Tire tire = new Tire(tirePressure, tireAge);
-                tires.add(tire);
-            }
-
-            Car car = new Car(model, engine, cargo, tires);
-            cars.add(car);
+            n--;
         }
 
-        String fragileOrFlammable = scanner.nextLine();
-        if (fragileOrFlammable.equals("fragile")) {
-            for (Car car : cars) {
-                car.getTires().forEach(tire -> {
-                    if (tire.isFlat() && car.getCargo().getCargoType().equals("fragile")) {
-                        output.add(car);
-                    }
-                });
-            }
-        } else {
-            for (Car car : cars) {
-                if (car.getCargo().getCargoType().equals("flamable") && car.getEngine().getEnginePower() > 250) {
-                    output.add(car);
-                }
-            }
-        }
-        for (Car car : output) {
-            System.out.println(car.getModel());
+        String printCriteria = scanner.nextLine();
+
+        if (printCriteria.equals("fragile")) {
+            carsList
+                    .stream()
+                    .filter(c -> c.getCargo().getType().equals(printCriteria))
+                    .filter(c -> c.getTires().stream().anyMatch(Tire::isFlat))
+                    .forEach(c -> System.out.println(c.getModel()));
+        } else if (printCriteria.equals("flamable")) {
+            carsList.stream()
+                    .filter(c -> c.getCargo().getType().equals(printCriteria))
+                    .filter(c -> c.getEngine().getPower() > 250)
+                    .forEach(c -> System.out.println(c.getModel()));
         }
     }
+
+    private static List<Tire> getTires(String[] parts) {
+        List<Tire> tires = new ArrayList<>();
+
+        for (int i = 5; i <= 12; i+=2) {
+            double tirePressure = Double.parseDouble(parts[i]);
+            int tireAge = Integer.parseInt(parts[i + 1]);
+
+            Tire tire = new Tire(tirePressure, tireAge);
+            tires.add(tire);
+        }
+        return tires;
+    }
 }
+
+

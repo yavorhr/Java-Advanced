7aad4eb65b430c0dca91192a2fd3cb536e1fb908
wolfie <yavorhristozov@gmail.com>
2023-12-30 12:04:50 +0200

    refact : 02. Company Roster - Main
    Re-write the Main class by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 05.  Defining classes/Lab/Exercise/02. Company Roster/Main.java
+++ 05.  Defining classes/Lab/Exercise/02. Company Roster/Main.java
@@ -1,61 +1,66 @@
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        HashMap<String, Department> departments = new HashMap<String, Department>(); //име на отдел -> ОТДЕЛ (име, служители)
-        int n = Integer.parseInt(scanner.nextLine());
-        for (int i = 0; i < n; i++) {
-            String [] input = scanner.nextLine().split("\\s+");
-            Employee employee;
-            String name = input[0];
-            double salary = Double.parseDouble(input[1]);
-            String position = input[2];
-            String department = input[3];
-            //6 parameters
-            if(input.length == 6) {
-                String email = input[4];
-                int age = Integer.parseInt(input[5]);
-                employee = new Employee(name, salary, position,department, email, age);
-            }
-            //4 parameters
-            else if (input.length == 4) {
-                employee = new Employee(name, salary, position, department);
-            }
-            //5 parameters
-            else {
-                try {
-                    int age = Integer.parseInt(input[4]);
-                    employee = new Employee(name, salary, position, department, age);
-                } catch (NumberFormatException e) {
-                    String email = input[4];
-                    employee = new Employee(name, salary, position, department, email);
-                }
-            }
+        int n = scanner.nextInt();
+        scanner.nextLine();
 
-            //служителя -> отдел
-            departments.putIfAbsent(department, new Department(department)); //създаваме си нов отдел
-            departments.get(department).getEmployees().add(employee);
+        HashMap<String, Department> departmentHashMap = new HashMap<>();
+
+        while (n > 0) {
+            String[] tokens = scanner.nextLine().split(" ");
+            String departmentName = tokens[3];
+
+            Employee employee = createEmployee(tokens);
+
+            departmentHashMap.putIfAbsent(departmentName, new Department(departmentName));
+            departmentHashMap.get(departmentName).getEmployeeList().add(employee);
+
+            n--;
         }
 
-        //Излизайки от for-цикъла имаме : име на отдела -> отдел (служители)
-
-        Department maxAverageSalaryDepartment  = departments.entrySet()
+        Department bestPaidDepartment = departmentHashMap
+                .entrySet()
                 .stream()
-                //кой е отдела с максимална средна заплата ? Взимаме отдела
-                .max(Comparator.comparingDouble(entry -> entry.getValue().getAverageSalary()))
-                //с .get() взимаме отдела (entry-то) с максимална средна заплата, който изчислихме с .max();
+                .max(Comparator.comparingDouble(d -> d.getValue().getAverageSalary()))
+                //get Map<String, Department>
                 .get()
-                //от това entry ми дай средната заплата
+                //get only Department as value from the map
                 .getValue();
 
-        System.out.println("Highest Average Salary: " + maxAverageSalaryDepartment.getName());
-        maxAverageSalaryDepartment.getEmployees()
+        printBestDepartment(bestPaidDepartment);
+    }
+
+    private static void printBestDepartment(Department bestPaidDepartment) {
+        System.out.println("Highest Average Salary: " + bestPaidDepartment.getName());
+        bestPaidDepartment.getEmployeeList()
                 .stream()
                 .sorted((e1, e2) -> Double.compare(e2.getSalary(), e1.getSalary()))
-                .forEach(employee -> System.out.println(employee.toString()));
+                .forEach(e -> System.out.println(e.toString()));
+    }
+
+    private static Employee createEmployee(String[] tokens) {
+        String name = tokens[0];
+        double salary = Double.parseDouble(tokens[1]);
+        String position = tokens[2];
+        String departmentName = tokens[3];
+
+        Employee employee = new Employee(name, salary, position, departmentName);
+
+        if (tokens.length == 5) {
+            String email = tokens[4];
+            employee.setEmail(email);
+        } else if (tokens.length == 6) {
+            String email = tokens[4];
+            int age = Integer.parseInt(tokens[5]);
+            employee.setEmail(email);
+            employee.setAge(age);
+        }
+
+        return employee;
     }
 }
+
+

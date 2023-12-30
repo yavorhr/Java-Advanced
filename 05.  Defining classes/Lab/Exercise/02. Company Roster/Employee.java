ca45dc173338f52c1759c04ab4886c0e22418e98
wolfie <yavorhristozov@gmail.com>
2023-12-30 12:05:56 +0200

    refact : 02. Company Roster - Employee
    Remove comments
    Add setters() and getters() for all fields

--- 05.  Defining classes/Lab/Exercise/02. Company Roster/Employee.java
+++ 05.  Defining classes/Lab/Exercise/02. Company Roster/Employee.java
@@ -1,17 +1,23 @@
 public class Employee {
-    //name, salary, position, department, email and age
-    //mandatory
     private String name;
     private double salary;
     private String position;
     private String department;
-    //optional
     private String email;
     private int age;
 
+    public Employee(String name, double salary, String position, String department, String email, int age) {
+        this(name, salary, position, department, email);
+        this.age = age;
+    }
 
-    //1. начин -> name, salary, position, department
-    public Employee (String name, double salary, String position, String department) {
+    public Employee(String name, double salary, String position, String department, String email) {
+        this(name, salary, position, department);
+        this.email = email;
+        this.age = -1;
+    }
+
+    public Employee(String name, double salary, String position, String department) {
         this.name = name;
         this.salary = salary;
         this.position = position;
@@ -19,32 +25,63 @@ public class Employee {
         this.email = "n/a";
         this.age = -1;
     }
-    //2. начин -> name,salary, position, department, email, age
-    public Employee (String name, double salary, String position, String department, String email, int age) {
-        this(name, salary, position, department);
-        this.email = email;
-        this.age = age;
+
+    public String getName() {
+        return this.name;
     }
-    //3. начин -> name, salary, position, department, email
-    public Employee (String name, double salary, String position, String department, String email) {
-        this(name, salary, position, department);
-        this.email = email;
-        this.age = -1;
-    }
-    //4. начин -> name, salary, position, department, age
-    public Employee (String name, double salary, String position, String department, int age) {
-        this(name, salary, position, department);
-        this.age = age;
-        this.email = "n/a";
+
+    public Employee setName(String name) {
+        this.name = name;
+        return this;
     }
 
     public double getSalary() {
         return this.salary;
     }
 
-    @Override
-    public String toString() {
-        return String.format("%s %.2f %s %d", this.name, this.salary, this.email, this.age);
+    public Employee setSalary(double salary) {
+        this.salary = salary;
+        return this;
     }
 
+    public String getPosition() {
+        return position;
+    }
+
+    public Employee setPosition(String position) {
+        this.position = position;
+        return this;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public Employee setDepartment(String department) {
+        this.department = department;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Employee setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Employee setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %.2f %s %d", getName(), getSalary(), getEmail(), getAge());
+    }
 }

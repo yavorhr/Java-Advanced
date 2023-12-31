3ff784df6982eb37a207294ab5378eca871ccbbd
wolfie <yavorhristozov@gmail.com>
2023-12-31 15:14:52 +0200

    refact : 06. Pokemon Trainer - Pokemon
    re-arrange code in Pokemon class
    add setters and getters

--- 05.  Defining classes/Lab/Exercise/06. Pokemon Trainer/Pokemon.java
+++ 05.  Defining classes/Lab/Exercise/06. Pokemon Trainer/Pokemon.java
@@ -1,25 +1,44 @@
-package Pockemon;
+import java.util.List;
 
 public class Pokemon {
-  private  String name;
-  private  String element;
-  private  int health;
+    private String name;
+    private String element;
+    private int health;
 
-    public Pokemon (String name, String element, int health) {
+    public Pokemon(String name, int health, String element) {
         this.name = name;
-        this.element=element;
-        this.health=health;
+        this.health = health;
+        this.element = element;
     }
 
-    public String getElement (){
-        return this.element;
+    public String getName() {
+        return name;
     }
 
-    public void takeDamage (int damage) {
-        this.health -=damage;
+    public Pokemon setName(String name) {
+        this.name = name;
+        return this;
     }
 
-    public int getHealth (){
-        return this.health;
+    public int getHealth() {
+        return health;
+    }
+
+    public Pokemon setHealth(int health) {
+        this.health = health;
+        return this;
+    }
+
+    public String getElement() {
+        return element;
+    }
+
+    public Pokemon setElement(String element) {
+        this.element = element;
+        return this;
+    }
+
+    public void takeDamage(int damage){
+        this.health -= damage;
     }
 }

70d6c0de667072ae1ca15b095b220295bdbc7be5
wolfie <yavorhristozov@gmail.com>
2024-01-01 15:33:47 +0200

    refact : 09. Cat Lady
    rename variables
    refact toString
    refact setters return type

--- 05.  Defining classes/Lab/Exercise/09. Cat Lady/Cat.java
+++ 05.  Defining classes/Lab/Exercise/09. Cat Lady/Cat.java
@@ -1,16 +1,32 @@
-package Pockemon;
-
 public class Cat {
     private String name;
-    private double specialChar;
+    private double specialCharacteristic;
 
-    public Cat(String name, double specialChar) {
+    public Cat(String name, double specialCharacteristic) {
         this.name = name;
-        this.specialChar = specialChar;
+        this.specialCharacteristic = specialCharacteristic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Cat setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public double getSpecialCharacteristic() {
+        return specialCharacteristic;
+    }
+
+    public Cat setSpecialCharacteristic(int specialCharacteristic) {
+        this.specialCharacteristic = specialCharacteristic;
+        return this;
     }
 
     @Override
     public String toString() {
-        return String.format("%s %.2f", this.name, this.specialChar);
+        return String.format("%s %.2f", getName(),getSpecialCharacteristic());
     }
 }

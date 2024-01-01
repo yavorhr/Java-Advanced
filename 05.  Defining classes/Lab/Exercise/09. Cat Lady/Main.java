01c1d5cd9feb7e51f492395a4f73a9944a60708e
wolfie <yavorhristozov@gmail.com>
2024-01-01 15:32:20 +0200

    refact : 09. Cat Lady - Main
    Re-write the Main class by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 05.  Defining classes/Lab/Exercise/09. Cat Lady/Main.java
+++ 05.  Defining classes/Lab/Exercise/09. Cat Lady/Main.java
@@ -1,4 +1,4 @@
-package Pockemon;
+import org.w3c.dom.ls.LSOutput;
 
 import java.util.*;
 
@@ -6,34 +6,44 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Map<String, Cat> cats = new HashMap<>();
+        Map<String, Cat> catsMap = new HashMap<>();
 
         String input = scanner.nextLine();
         while (!"End".equals(input)) {
-            String[] tokens = input.split("\\s+");
+            Cat cat = getCat(input);
 
-            String name = tokens[1];
-            double specialChar = Double.parseDouble(tokens[2]);
-            Cat cat = null;
-
-            if (input.contains("Siamise")) {
-                cat = new Siamese(name, specialChar);
-            } else if (input.contains("Cymric")) {
-                cat = new Cymric(name, specialChar);
-            } else {
-                cat = new StreetExtraordinaire(name, specialChar);
-            }
-            cats.put(name, cat);
+            catsMap.put(cat.getName(), cat);
 
             input = scanner.nextLine();
         }
 
-        String catName = scanner.next();
-        System.out.println(cats.get(catName).toString());
+        input = scanner.nextLine();
+        printCat(catsMap, input);
     }
 
+    private static void printCat(Map<String, Cat> catsMap, String input) {
+        Cat currentCat = catsMap.get(input);
+        if (currentCat != null) {
+            String breed = currentCat.getClass().getName();
+            System.out.printf("%s %s", breed, currentCat.toString());
+        }
+    }
+
+    private static Cat getCat(String input) {
+        String[] tokens = input.split(" ");
+        String breed = tokens[0];
+        String name = tokens[1];
+        double specialCharacteristic = Double.parseDouble(tokens[2]);
+
+        Cat cat = null;
+
+        switch (breed) {
+            case "StreetExtraordinaire" -> cat = new StreetExtraordinaire(name, specialCharacteristic);
+            case "Siamese" -> cat = new Siamese(name, specialCharacteristic);
+            case "Cymric" -> cat = new Cymric(name, specialCharacteristic);
+        }
+        return cat;
+    }
 }
-
-
 
 

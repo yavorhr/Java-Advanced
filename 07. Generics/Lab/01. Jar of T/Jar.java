b3c0d98947a7e6a1845f5b99b450d7a782a7dda7
wolfie <yavorhristozov@gmail.com>
2024-01-04 16:47:38 +0200

    refact : 01. Jar of T - Jar
    add toString for the class

--- 07. Generics/Lab/01. Jar of T/Jar.java
+++ 07. Generics/Lab/01. Jar of T/Jar.java
@@ -1,17 +1,39 @@
 import java.util.ArrayDeque;
 
-public class Jar<Type> {
-    private ArrayDeque<Type> elements;
+public class Jar <T>{
+    private ArrayDeque<T> jarStack;
 
     public Jar() {
-        this.elements = new ArrayDeque<>();
+        this.jarStack = new ArrayDeque<>();
     }
 
-    public void add(Type element) {
-        this.elements.push(element);
+    public void add(T element){
+        this.jarStack.push(element);
     }
 
-    public Type remove() {
-        return this.elements.pop();
+    public void remove() {
+        this.jarStack.pop();
+    }
+
+    public ArrayDeque<T> getJarStack() {
+        return jarStack;
+    }
+
+    public Jar<T> setJarStack(ArrayDeque<T> jarStack) {
+        this.jarStack = jarStack;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", getDeck(getJarStack()));
+    }
+
+    private String getDeck(ArrayDeque<T> jarStack) {
+        StringBuilder sb = new StringBuilder();
+
+        jarStack.forEach(e -> sb.append(e.toString()).append(" "));
+
+        return sb.toString();
     }
 }

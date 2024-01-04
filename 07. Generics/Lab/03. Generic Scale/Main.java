45d47c38e1c129a67d9224cadfee4d30d8eb3ee6
wolfie <yavorhristozov@gmail.com>
2024-01-04 15:02:32 +0200

    refact : 03. Scale - Main class
    Showing how String, Integer and Character types can be compared with one single generic method - getHeavier().
    In addition, Person objects are created, and are also compared for the purpose of the demo

--- 07. Generics/Lab/03. Generic Scale/Main.java
+++ 07. Generics/Lab/03. Generic Scale/Main.java
@@ -1,12 +1,21 @@
-import java.lang.reflect.Array;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scale <String> scale = new Scale<>("1","-1");
-        System.out.println(scale.getHeavier()   );
+        Scale<Integer> scaleInts = new Scale<>(50, 60);
+        Scale<String> scaleString = new Scale<>("50", "600");
+        Scale<Character> scaleCharacters = new Scale<>('8', '9');
 
+        System.out.println(scaleInts.getHeavier());
+        System.out.println(scaleString.getHeavier());
+        System.out.println(scaleCharacters.getHeavier());
 
+        Person person = new Person("Todor", 200);
+        Person otherPerson = new Person("Momchil", 300);
+
+        Scale<Person> scalePerson = new Scale<>(person ,otherPerson);
+        System.out.printf("Heavier person is : %s", scalePerson.getHeavier());
     }
 }
+

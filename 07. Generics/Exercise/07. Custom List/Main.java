23d089beec2fe52fc5226f0b874966ccbdb9b4fb
wolfie <yavorhristozov@gmail.com>
2024-01-08 14:15:48 +0200

    refact : 07. CustomList - Main
    Minor changes in syntax, functionality is inchanged

--- 07. Generics/Exercise/07. Custom List/Main.java
+++ 07. Generics/Exercise/07. Custom List/Main.java
@@ -4,49 +4,42 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        CustomList<String> customList = new CustomList();
+        CustomList<String> customList = new CustomList<>();
 
         String input = scanner.nextLine();
-        while (!"END".equals(input)) {
-            String[] tokens = input.split("\\s+");
+        while (!input.equals("END")) {
+            String[] tokens = input.split(" ");
             String command = tokens[0];
+
             switch (command) {
-                case "Add":
-                    String element = tokens[1];
-                    customList.add(element);
-                    break;
-                case "Remove":
+                case "Add" -> {
+                    String charSequence = tokens[1];
+                    customList.add(charSequence);
+                }
+                case "Remove" -> {
                     int index = Integer.parseInt(tokens[1]);
                     customList.remove(index);
-                    break;
-                case "Contains":
+                }
+                case "Contains" -> {
                     String searchElement = tokens[1];
                     System.out.println(customList.contains(searchElement));
-                    break;
-                case "Swap":
+                }
+                case "Swap" -> {
                     int firstIndex = Integer.parseInt(tokens[1]);
                     int secondIndex = Integer.parseInt(tokens[2]);
                     customList.swap(firstIndex, secondIndex);
-                    break;
-                case "Greater":
+                }
+                case "Greater" -> {
                     String elementToCompare = tokens[1];
                     System.out.println(customList.countGreaterThan(elementToCompare));
-                    break;
-                case "Max":
-                    System.out.println(customList.getMax());
-                    break;
-                case "Min":
-                    System.out.println(customList.getMin());
-                    break;
-                case "Print":
-                    System.out.println(customList.toString());
-                    break;
-                default:
-                    System.out.println("Invalid command");
-                    break;
+                }
+                case "Max" -> System.out.println(customList.getMax());
+                case "Min" -> System.out.println(customList.getMin());
+                case "Print" -> System.out.println(customList.toString());
+                default -> System.out.println("Invalid command");
             }
             input = scanner.nextLine();
         }
-
     }
 }
+

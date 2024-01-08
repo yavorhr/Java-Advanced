ff46fddae777f861bebbd9f731ca826f35bd8d33
wolfie <yavorhristozov@gmail.com>
2024-01-08 13:34:57 +0200

    refact : 06. Generic Count Method Doubles - Main
    minor changes, functionality remains the same

--- 07. Generics/Exercise/06. Generic Count Method Double/Main.java
+++ 07. Generics/Exercise/06. Generic Count Method Double/Main.java
@@ -4,15 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
-        Box<Double> box = new Box();
-        for (int i = 0; i < n; i++) {
-            double number = Double.parseDouble(scanner.nextLine());
-            box.add(number);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        Box<Double> box = new Box<>();
+
+        while (n > 0) {
+            double input = Double.parseDouble(scanner.nextLine());
+            box.add(input);
+            n--;
         }
 
-        double elementToCompare = Double.parseDouble(scanner.nextLine());
-        int countGreater = box.countGreaterThan(elementToCompare);
-        System.out.println(countGreater);
+        double doubleInput = Double.parseDouble(scanner.nextLine());
+        int countElements = box.getGreaterCountOfElements(doubleInput);
+
+        printGreaterElementsCount(countElements);
     }
+
+    private static void printGreaterElementsCount(int count) {
+        System.out.println(count);
+    }
+
 }
+

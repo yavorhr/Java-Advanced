c3512a2fbc0ac37262fab9915216930ef625f8c7
wolfie <yavorhristozov@gmail.com>
2024-01-08 12:08:00 +0200

    refact : 03. Generic Swap Method String - Main
    Extend Main code by adding :
    1. Indexes validity check
    2. Separate methods for splitting the Main class

--- 07. Generics/Exercise/03. Generic Swap Method String/Main.java
+++ 07. Generics/Exercise/03. Generic Swap Method String/Main.java
@@ -1,20 +1,42 @@
-
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
-        Box<String> box = new Box();
-        for (int i = 0; i < n; i++) {
-            String text = scanner.nextLine();
-            box.add(text);
+        int n = readNumber(scanner);
+        Box<String> box = new Box<>();
+
+        while (n > 0) {
+            String input = scanner.nextLine();
+            box.add(input);
+            n--;
         }
+
         int firstIndex = scanner.nextInt();
-        int secondIndex = scanner.nextInt();
-        box.swap(firstIndex,secondIndex);
-        System.out.println(box.toString());
+        int secondIndex = readNumber(scanner);
+
+        if (isValid(firstIndex, box.getSize()) && isValid(secondIndex, box.getSize())) {
+            box.swapElements(firstIndex, secondIndex);
+        } else {
+            System.out.println("Invalid indexes!");
+        }
+
+        printBoxElements(box);
+    }
+
+    private static void printBoxElements(Box<String> box) {
+        System.out.println(box);
+    }
+
+    private static boolean isValid(int index, int size) {
+        return index >= 0 && index < size;
+    }
+
+    private static int readNumber(Scanner scanner) {
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        return n;
     }
 }
+

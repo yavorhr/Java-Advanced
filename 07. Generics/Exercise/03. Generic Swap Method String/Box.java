48f76eb2707fc206bdc0ad2c38154f57bf27b267
wolfie <yavorhristozov@gmail.com>
2024-01-08 12:06:46 +0200

    refact : 03. Generic Swap Method String - Box
    Rename some variables
    Add additional support methods such as setters, getters, getSize()
    
    Functionality remains the same

--- 07. Generics/Exercise/03. Generic Swap Method String/Box.java
+++ 07. Generics/Exercise/03. Generic Swap Method String/Box.java
@@ -2,34 +2,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Box<T> {
-    private List<T> values;
+    private List<T> elements;
 
     public Box() {
-        this.values = new ArrayList<>();
+        this.elements = new ArrayList<>();
     }
 
     public void add(T element) {
-        this.values.add(element);
+        this.elements.add(element);
     }
 
-    public void swap(int first, int second) {
-        T firstElement = this.values.get(first);
-        T secondElement = this.values.get(second);
-        for (int i = 0; i < values.size(); i++) {
-            values.set(first, secondElement);
-            values.set(second, firstElement);
-        }
+    public List<T> getElements() {
+        return elements;
+    }
+
+    public Box<T> setElements(List<T> elements) {
+        this.elements = elements;
+        return this;
+    }
+
+    public int getSize() {
+        return this.elements.size();
+    }
+
+    public void swapElements(int firstIndex, int secondIndex) {
+        T firstElement = getElements().get(firstIndex);
+        T secondElement = getElements().get(secondIndex);
+
+        this.elements.set(secondIndex,firstElement);
+        this.elements.set(firstIndex,secondElement);
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (T value : this.values) {
-            sb.append(String.format("%s: %s", value.getClass().getName(), value)).append(System.lineSeparator());
-        }
+        getElements().forEach(e -> sb.append(String.format("%s: %s\n", e.getClass(), e)));
         return sb.toString();
     }
+
 }
-
-
-

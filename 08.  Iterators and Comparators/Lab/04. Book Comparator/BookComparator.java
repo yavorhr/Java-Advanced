17b6d910865f257563e81a421af2d3cbe555a774
wolfie <yavorhristozov@gmail.com>
2024-01-11 14:15:32 +0200

    refact : 04. Book Comparator
    just for practice

--- 08.  Iterators and Comparators/Lab/04. Book Comparator/BookComparator.java
+++ 08.  Iterators and Comparators/Lab/04. Book Comparator/BookComparator.java
@@ -3,10 +3,10 @@ import java.util.Comparator;
 public class BookComparator implements Comparator<Book> {
 
     @Override
-    public int compare(Book first, Book second) {
-        int result = first.compareTo(second);
+    public int compare(Book firstBook, Book secondBook) {
+        int result = firstBook.compareTo(secondBook);
         if (result == 0) {
-            result = Integer.compare(first.getYear(), second.getYear());
+            result = Integer.compare(firstBook.getYear(), secondBook.getYear());
         }
         return result;
     }

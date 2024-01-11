62e8dd3a01fca4663f921bb093490cc468a29ac2
wolfie <yavorhristozov@gmail.com>
2024-01-11 10:03:04 +0200

    refact : 02. Library
    Exercise, no changes performed

--- 08.  Iterators and Comparators/Lab/02. Library/Main.java
+++ 08.  Iterators and Comparators/Lab/02. Library/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
         Book bookThree = new Book("The Documents in the Case", 2002);
         Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
@@ -7,10 +8,9 @@ public class Main {
         Library library = new Library(bookOne, bookTwo, bookThree);
 
         for (Book book : library) {
-            System.out.println(book.getTitle());
+            System.out.println(book);
         }
 
     }
 }
-
 

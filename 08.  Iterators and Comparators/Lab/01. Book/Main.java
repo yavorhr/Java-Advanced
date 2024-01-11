a98adf6c48d136fe139a7f9205ee48cc4a6f0b51
wolfie <yavorhristozov@gmail.com>
2024-01-11 09:35:19 +0200

    refact : 01. Book
    Minor changes, functionality remains the same

--- 08.  Iterators and Comparators/Lab/01. Book/Main.java
+++ 08.  Iterators and Comparators/Lab/01. Book/Main.java
@@ -1,23 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
-            Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
-            Book bookThree = new Book("The Documents in the Case", 2002);
 
-            List<Book> books = new ArrayList<>();
-            books.add(bookOne);
-            books.add(bookTwo);
-            books.add(bookThree);
+        Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        Book bookThree = new Book("The Documents in the Case", 2002);
+        Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
+
+        List<Book> books = new ArrayList<>();
+
+        books.add(bookOne);
+        books.add(bookTwo);
+        books.add(bookThree);
 
         for (Book book : books) {
-            System.out.println(book.getAuthors().size());
-        }
-
+            System.out.println(book);;
         }
     }
+}
 

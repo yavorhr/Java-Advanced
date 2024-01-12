da7cf33ea30dc3ef6e1911af16ec3de983b665a3
wolfie <yavorhristozov@gmail.com>
2024-01-12 16:34:59 +0200

    refact : 01. ListIterator - Main
    Minor changes in reading data from the console
    MAin functionaltiy remains the same

--- 08.  Iterators and Comparators/Exercise/01. ListyIterator/Main.java
+++ 08.  Iterators and Comparators/Exercise/01. ListyIterator/Main.java
@@ -1,31 +1,42 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> data = Arrays.stream(scanner.nextLine().split("\\s+")).skip(1)
-                .collect(Collectors.toCollection(ArrayList::new));
 
-        ListyIterator listyIterator = new ListyIterator(data);
-        String command = scanner.nextLine();
-        while (!"END".equals(command)) {
+        ListIterator listIterator = null;
+
+        String input = scanner.nextLine();
+        while (!"END".equals(input)) {
+            String[] tokens = input.split(" ");
+
+            String command = tokens[0];
             switch (command) {
-                case "Create":
-                    break;
-                case "Move":
-                    System.out.println(listyIterator.move());
-                    break;
-                case "Print":
-                    System.out.println(listyIterator.print());
-                    break;
-                case "HasNext":
-                    System.out.println(listyIterator.hasNext());
-                    break;
+                case "Create" -> {
+                    if (tokens.length == 1) {
+                        listIterator = new ListIterator();
+                    } else {
+                        List<String> names = Arrays.stream(tokens).skip(1).collect(Collectors.toList());
+                        listIterator = new ListIterator(names);
+                    }
+                }
+                case "Move" -> System.out.println(listIterator.move());
+
+                case "HasNext" -> System.out.println(listIterator.hasNext());
+
+                case "Print" -> System.out.println(listIterator.print());
+
             }
-            command = scanner.nextLine();
+            input = scanner.nextLine();
         }
+
     }
+
 }
+
 
 

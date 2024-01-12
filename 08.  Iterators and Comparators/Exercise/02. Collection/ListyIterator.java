608914a19ee64336f494b9ebe64cbb49dacfdd70
wolfie <yavorhristozov@gmail.com>
2024-01-12 17:00:27 +0200

    refact : 02. Collection - ListIterator
    Practice

--- 08.  Iterators and Comparators/Exercise/02. Collection/ListyIterator.java
+++ 08.  Iterators and Comparators/Exercise/02. Collection/ListyIterator.java
@@ -1,52 +1,65 @@
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class ListyIterator implements Iterable<String> {
-    List<String> data;
-    int index;
+public class ListIterator implements Iterable<String> {
+    private List<String> data;
+    private int index;
 
-    public ListyIterator(List<String> data) {
-        this.data = data;
-        if (data.size() != 0) {
+    public ListIterator(List<String> list) {
+        this.data = list;
+        setIndex();
+    }
+
+    public ListIterator() {
+        this.data = new ArrayList<>();
+        setIndex();
+    }
+
+    public boolean move() {
+        if (this.index < this.data.size() - 1) {
+            this.index++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasNext() {
+        return this.index < this.data.size() - 1;
+    }
+
+    public String print() {
+        if (this.index == -1) {
+            return "Invalid Operation!";
+        }
+        return this.data.get(this.index);
+    }
+
+    private void setIndex() {
+        if (this.data.size() != 0) {
             this.index = 0;
         } else {
             this.index = -1;
         }
     }
 
-    public boolean move() {
-        if (this.index < this.data.size() - 1) {
-            index++;
-            return true;
-        }
-        return false;
-    }
+    private class InnerIterator implements Iterator<String> {
+        private int index = 0;
 
-    public String print() {
-        if (this.index == -1) {
-            //празен списък
-            return "Invalid Operation!";
+        @Override
+        public boolean hasNext() {
+            return this.index <= data.size() - 1;
         }
-        return this.data.get(this.index);
+
+        @Override
+        public String next() {
+            return data.get(this.index++);
+        }
     }
 
     @Override
     public Iterator<String> iterator() {
-        return new Iterator<String>() {
-            private int index = 0;
-
-            @Override
-            public boolean hasNext() {
-                return this.index <= data.size() - 1;
-            }
-
-            @Override
-            public String next() {
-                return data.get(this.index++);
-            }
-        };
+        return new InnerIterator();
     }
 }
-
-
 

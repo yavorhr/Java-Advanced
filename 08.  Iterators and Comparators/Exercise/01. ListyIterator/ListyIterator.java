ba106f28a12cf8d7d28088a5fed0dd85418d79fb
wolfie <yavorhristozov@gmail.com>
2024-01-12 16:37:41 +0200

    refact : 01. ListIterator - ListIterator
    Change fields access modifiers to private
    Add second default Constructor for creating ListIterator object
    Move the check for the index from the constructor in setIndex() method
    Remove comments

--- 08.  Iterators and Comparators/Exercise/01. ListyIterator/ListyIterator.java
+++ 08.  Iterators and Comparators/Exercise/01. ListyIterator/ListyIterator.java
@@ -1,38 +1,46 @@
+import java.util.ArrayList;
 import java.util.List;
 
-public class ListyIterator {
-    List<String> data;
-    int index;
+public class ListIterator {
+    private List<String> data;
+    private int index;
 
-    public ListyIterator(List<String> data) {
-        this.data = data;
-        if (data.size() != 0) {
+    public ListIterator(List<String> list) {
+        this.data = list;
+        setIndex();
+    }
+
+    public ListIterator() {
+        this.data = new ArrayList<>();
+        setIndex();
+    }
+
+    public boolean move() {
+        if (this.index < this.data.size() - 1) {
+            this.index++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasNext() {
+        return this.index < this.data.size() - 1;
+    }
+
+    public String print() {
+        if (this.index == -1) {
+            return "Invalid Operation!";
+        }
+        return this.data.get(this.index);
+    }
+
+    private void setIndex() {
+        if (this.data.size() != 0) {
             this.index = 0;
         } else {
             this.index = -1;
         }
     }
 
-    public boolean move() {
-        if (this.index < this.data.size() - 1) {
-            index++;
-            return true;
-        }
-        return false;
-    }
-
-    public String print() {
-        if (this.index == -1) {
-            //празен списък
-            return "Invalid Operation!";
-        }
-        return this.data.get(this.index);
-    }
-
-    public boolean hasNext() {
-        return this.index < this.data.size() - 1;
-    }
 }
-
-
 

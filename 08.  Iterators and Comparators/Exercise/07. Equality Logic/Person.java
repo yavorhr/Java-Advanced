43d5d8d24b380585713ea2c62f76c7e6b8dee150
wolfie <yavorhristozov@gmail.com>
2024-01-14 14:55:31 +0200

    add: 07. Equality Logic
    Add Person class

---
+++ 08.  Iterators and Comparators/Exercise/07. Equality Logic/Person.java
@@ -0,0 +1,61 @@
+import java.util.Objects;
+
+public class Person implements Comparable<Person> {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Person setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        int result = this.name.compareTo(other.name);
+        if (result == 0) {
+            result = Integer.compare(this.age, other.age);
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d", this.name, this.age);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}

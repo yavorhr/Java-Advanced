4f09bd571eeeace30460a98fc8637be74e4edc96
wolfie <yavorhristozov@gmail.com>
2024-01-14 10:55:45 +0200

    refact : 03. Stack Iterator - Main
    Add code to Main to test the Stack Iterator

--- 08.  Iterators and Comparators/Exercise/03. Stack Iterator/Main.java
+++ 08.  Iterators and Comparators/Exercise/03. Stack Iterator/Main.java
@@ -1,10 +1,19 @@
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+        Stack stack = new Stack();
+        stack.push(32);
+        stack.push(50);
+        stack.push(69);
+        stack.push(99);
+
+        Iterator<Integer> iterator = stack.iterator();
+
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());;
+        }
 
     }
 }

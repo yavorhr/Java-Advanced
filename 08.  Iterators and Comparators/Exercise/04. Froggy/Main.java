abfaf7b0fccd63be283d97047e4641cc243b156d
wolfie <yavorhristozov@gmail.com>
2024-01-14 13:22:59 +0200

    refact : 04. Froggy - Main
    Practicing Iterators
    No major changes in code

--- 08.  Iterators and Comparators/Exercise/04. Froggy/Main.java
+++ 08.  Iterators and Comparators/Exercise/04. Froggy/Main.java
@@ -3,17 +3,29 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] stonesNumbers = Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).toArray();
 
-        Lake lake = new Lake(stonesNumbers);
+        int[] numbers =
+                Arrays.stream(scanner.nextLine()
+                        .split(", "))
+                        .mapToInt(Integer::parseInt).toArray();
 
-        StringBuilder sb = new StringBuilder();
+        Lake lake = new Lake(numbers);
 
-        for (Integer integer : lake) {
-            sb.append(integer).append(", ");
+        Iterator<Integer> iterator = lake.iterator();
+
+        printResult(iterator);
+    }
+
+    private static void printResult(Iterator<Integer> iterator) {
+        StringBuilder result = new StringBuilder();
+
+        while (iterator.hasNext()) {
+            result.append(String.format("%d, ", iterator.next()));
         }
-        System.out.println(sb.toString().substring(0, sb.toString().lastIndexOf(", ")));
+
+        System.out.println(result.substring(0, result.toString().lastIndexOf(", ")));
     }
 }
+
 
 

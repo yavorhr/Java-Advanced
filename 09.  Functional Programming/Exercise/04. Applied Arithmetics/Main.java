c1aabdd2d14d0601176b9205650f62556ca63253
wolfie <yavorhristozov@gmail.com>
2024-01-20 16:19:46 +0200

    refact : 04. Applier Arithmetics
    Practice Functions and Consumers

--- 09.  Functional Programming/Exercise/04. Applied Arithmetics/Main.java
+++ 09.  Functional Programming/Exercise/04. Applied Arithmetics/Main.java
@@ -6,33 +6,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Function<int[], int[]> add = arr -> Arrays.stream(arr).map(e -> e += 1).toArray();
-        Function<int[], int[]> multiply = arr -> Arrays.stream(arr).map(e -> e *= 2).toArray();
-        Function<int[], int[]> subtract = arr -> Arrays.stream(arr).map(e -> e -= 1).toArray();
-        Consumer<int[]> print = arr -> Arrays.stream(arr).forEach(e -> System.out.print(e + " "));
+        Function<int[], int[]> addConsumer = arr -> Arrays.stream(arr).map(e -> e += 1).toArray();
+        Function<int[], int[]> multiplyConsumer = arr -> Arrays.stream(arr).map(e -> e *= 2).toArray();
+        Function<int[], int[]> subtractConsumer = arr -> Arrays.stream(arr).map(e -> e -= 1).toArray();
+        Consumer<int[]> printConsumer = arr -> Arrays.stream(arr).forEach(e -> System.out.print(e + " "));
 
-        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        int[] numbersInput = Arrays.stream(scanner.nextLine()
+                .split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
         String command = scanner.nextLine();
-        while (!command.equals("end")) {
+        while (!"end".equals(command)) {
             switch (command) {
-                case "add":
-                    numbers = add.apply(numbers);
-                    break;
-                case "multiply":
-                    numbers = multiply.apply(numbers);
-                    break;
-                case "subtract":
-                    numbers = subtract.apply(numbers);
-                    break;
-                case "print":
-                    print.accept(numbers);
-                    System.out.println();
-                    break;
+                case "add" -> numbersInput = addConsumer.apply(numbersInput);
+                case "multiply" -> numbersInput = multiplyConsumer.apply(numbersInput);
+                case "subtract" -> numbersInput = subtractConsumer.apply(numbersInput);
+                case "print" -> printConsumer.accept(numbersInput);
             }
             command = scanner.nextLine();
         }
 
     }
 }
+
+
+
 

48638bb071713f5bcb459b23e02c98a1b668dfd2
wolfie <yavorhristozov@gmail.com>
2024-01-20 16:58:55 +0200

    refact : 08. Custom Comparator
    Practice Comparator
    No major changes in code

--- 09.  Functional Programming/Exercise/08. Custom Comparator/Main.java
+++ 09.  Functional Programming/Exercise/08. Custom Comparator/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+        List<Integer> collect = Arrays.stream(scanner.nextLine()
+                .split(" "))
                 .mapToInt(Integer::parseInt)
                 .boxed()
                 .collect(Collectors.toList());
@@ -13,15 +14,17 @@ public class Main {
         Comparator<Integer> customComparator = ((a, b) -> {
             if (a % 2 == 0 && b % 2 == 0) {
                 return a.compareTo(b);
-            } else if (a % 2 == 0 && b % 2 != 0) {
+            } else if (a % 2 == 0) {
                 return -1;
-            } else if (a % 2 != 0 && b % 2 == 0) {
+            } else if (b % 2 == 0) {
                 return 1;
             }
             return a.compareTo(b);
         });
 
-        numbers.stream().sorted(customComparator).forEach(e -> System.out.print(e + " "));
+        collect.stream().
+                sorted(customComparator)
+                .forEach(n -> System.out.print(n+ " "));
+
     }
 }
-

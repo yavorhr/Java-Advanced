2d7b0a6c74468ea1f91e7b9b6f2eb3f8753a757d
wolfie <yavorhristozov@gmail.com>
2024-01-20 10:27:14 +0200

    refact : 01. Sort Even Numbers
    Practice Lambda functions

--- 09.  Functional Programming/Lab/01. Sort Even Numbers/Main.java
+++ 09.  Functional Programming/Lab/01. Sort Even Numbers/Main.java
@@ -1,28 +1,35 @@
 import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.nextLine();
+        Predicate<Integer> filterEvenNumbers = e -> e % 2 == 0;
 
-        String output = Arrays.stream(input.split(", "))
+        String result = Arrays.stream(scanner.nextLine()
+                .split(", "))
                 .mapToInt(Integer::parseInt)
-                .filter(num -> num % 2 == 0)
+                .filter(filterEvenNumbers::test)
                 .mapToObj(String::valueOf)
                 .collect(Collectors.joining(", "));
 
-        System.out.println(output);
+        printResult(result);
 
-        output = Arrays.stream(input.split(", "))
+        result = Arrays.stream(result
+                .split(", "))
                 .mapToInt(Integer::parseInt)
-                .filter(num -> num % 2 == 0)
                 .sorted()
                 .mapToObj(String::valueOf)
                 .collect(Collectors.joining(", "));
 
-        System.out.println(output);
+        printResult(result);
+    }
+
+    private static void printResult(String result) {
+        System.out.println(result);
     }
 }
 

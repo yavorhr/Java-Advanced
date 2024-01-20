0f7332f0fc0aa7a07d3efabab2640ed156bca5eb
wolfie <yavorhristozov@gmail.com>
2024-01-20 16:36:34 +0200

    refact : 06 Predicate for Names
    Practice Predicate & Consumer
    No major changes in code

--- 09.  Functional Programming/Exercise/06. Predicate For Names/Main.java
+++ 09.  Functional Programming/Exercise/06. Predicate For Names/Main.java
@@ -1,18 +1,26 @@
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        int allowedLength = Integer.parseInt(scanner.nextLine());
 
-        String[] names = scanner.nextLine().split("\\s+");
-        Predicate<String> checkLength = name -> name.length() <= n;
+        String[] names = Arrays.stream(scanner.nextLine()
+                .split(" "))
+                .toArray(String[]::new);
+
+        Predicate<String> filterByLength = e -> e.length() <= allowedLength;
+        Consumer<String> printer = System.out::println;
 
         Arrays.stream(names)
-                .filter(checkLength)
-                .forEach(System.out::println);
+                .filter(filterByLength)
+                .forEach(printer);
     }
 }
+
+
+
 

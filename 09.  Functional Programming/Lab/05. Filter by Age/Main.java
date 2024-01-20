cf70ce3d3a8486385c18ed84abf274b7b40dabd5
wolfie <yavorhristozov@gmail.com>
2024-01-20 13:26:11 +0200

    refact : 05. Filter by Age
    Practice Predicate and Function

--- 09.  Functional Programming/Lab/05. Filter by Age/Main.java
+++ 09.  Functional Programming/Lab/05. Filter by Age/Main.java
@@ -4,13 +4,19 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Main {
-    public static class Person {
-        String name;
-        int age;
 
-        Person(String name, int age) {
+    public static class Person {
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
             this.name = name;
             this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s - %d", this.name, this.age);
         }
     }
 
@@ -19,41 +25,47 @@ public class Main {
 
         int n = Integer.parseInt(scanner.nextLine());
         List<Person> people = new ArrayList<>();
+
         while (n-- > 0) {
             String[] tokens = scanner.nextLine().split(", ");
             String name = tokens[0];
             int age = Integer.parseInt(tokens[1]);
-            Person person = new Person(name, age);
-            people.add(person);
+            people.add(new Person(name, age));
         }
 
         String criteria = scanner.nextLine();
         int ageCriteria = Integer.parseInt(scanner.nextLine());
-        Predicate<Person> ageFilter = createAgeFIlter(criteria, ageCriteria);
-
         String formatOption = scanner.nextLine();
 
+        Predicate<Person> ageFilter = createFilterPredicate(criteria, ageCriteria);
         Function<Person, String> formatter = createFormatter(formatOption);
-        System.out.println(people.stream()
+
+        String output = people
+                .stream()
                 .filter(ageFilter)
                 .map(formatter)
-                .collect(Collectors.joining(System.lineSeparator())));
+                .collect(Collectors.joining(System.lineSeparator()));
+
+        System.out.println(output);
     }
 
-    private static Function<Person, String> createFormatter(String formatOption) {
-        if (formatOption.equals("name")) {
-            return p -> p.name;
-        } else if (formatOption.equals("age")) {
-            return p -> String.valueOf(p.age);
-        }
-        return p -> p.name + " - " + p.age;
-    }
-
-    private static Predicate<Person> createAgeFIlter(String criteria, int ageCriteria) {
+    public static Predicate<Person> createFilterPredicate(String criteria, int ageCriteria) {
         if (criteria.equals("older")) {
             return p -> p.age >= ageCriteria;
         }
         return p -> p.age <= ageCriteria;
     }
+
+    public static Function<Person, String> createFormatter(String formatOption) {
+        if (formatOption.equals("name")) {
+            return p -> p.name;
+        } else if (formatOption.equals("age")) {
+            return p -> String.valueOf(p.age);
+        }
+        return Person::toString;
+    }
 }
+
+
+
 

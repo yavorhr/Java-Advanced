6cb2ebca800af24fb20847a8c201cb7a8fe3f9d6
wolfie <yavorhristozov@gmail.com>
2024-01-27 15:29:14 +0200

    refact : 01.  Bee
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 11. Exams Practice/2. Matrices/02. Bee_Description/Main.java
+++ 11. Exams Practice/2. Matrices/02. Bee_Description/Main.java
@@ -1,191 +1,137 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static int beeRow = 0;
     public static int beeCol = 0;
-    public static int flowers = 0;
-    public static boolean gotLost = false;
+    public static int flowersCount = 0;
+    public static int NEEDED_FLOWERS = 5;
+    public static boolean isLost = false;
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
-        int size = Integer.parseInt(scanner.nextLine());
-        char[][] matrix = new char[size][size];
-        for (int row = 0; row < matrix.length; row++) {
-            String line = scanner.nextLine();
-            if (line.contains("B")) {
-                beeRow = row;
-                beeCol = line.indexOf("B");
-            }
-            for (int col = 0; col < matrix[row].length; col++) {
-                matrix[row][col] = line.charAt(col);
-            }
-        }
-        String input = scanner.nextLine();
-        while (!"End".equals(input)) {
-            if (input.equals("up")) {
-                moveBeeUp(beeRow - 1, beeCol, matrix);
-                if (gotLost) {
-                    matrix[beeRow][beeCol] = '.';
-                    break;
-                }
-            } else if (input.equals("down")) {
-                moveDown(beeRow + 1, beeCol, matrix);
-                if (gotLost) {
-                    matrix[beeRow][beeCol] = '.';
-                    break;
-                }
-            } else if (input.equals("right")) {
-                moveRight(beeRow, beeCol + 1, matrix);
-                if (gotLost) {
-                    matrix[beeRow][beeCol] = '.';
-                    break;
-                }
-            } else if (input.equals("left")) {
-                moveLeft(beeRow, beeCol - 1, matrix);
-                if (gotLost) {
-                    matrix[beeRow][beeCol] = '.';
-                    break;
-                }
-            }
-            input = scanner.nextLine();
-        }
+        int n = scanner.nextInt();
+        scanner.nextLine();
 
-        if (flowers >= 5) {
-            System.out.println(String.format("Great job, the bee manage to pollinate %d flowers!", flowers));
-        } else {
-            System.out.println(String.format("The bee couldn't pollinate the flowers, she needed %d flowers more", 5 - flowers));
+        char[][] matrix = readMatrix(scanner, n);
+        findBeeCoordinates(matrix);
+
+        String moveDirection = scanner.nextLine();
+        while (!"End".equals(moveDirection)) {
+            moveBee(matrix, moveDirection, beeRow, beeCol);
+
+            if (beeIsLost()) {
+                break;
+            }
+            moveDirection = scanner.nextLine();
         }
         printMatrix(matrix);
+    }
 
+    private static void moveBee(char[][] matrix, String moveDirection, int beeRow, int beeCol) {
+        switch (moveDirection) {
+            case "up" -> moveUp(matrix, beeRow, beeCol);
+            case "down" -> moveDown(matrix, beeRow, beeCol);
+            case "right" -> moveRight(matrix, beeRow, beeCol);
+            case "left" -> moveLeft(matrix, beeRow, beeCol);
+        }
+    }
+
+    private static void moveLeft(char[][] matrix, int row, int col) {
+        matrix[row][col] = '.';
+        int newCol = col - 1;
+        updateCoordinates(matrix, row, newCol, "left");
+    }
+
+    private static void moveRight(char[][] matrix, int row, int col) {
+        matrix[row][col] = '.';
+        int newCol = col + 1;
+        updateCoordinates(matrix, row, newCol, "right");
+    }
+
+    private static void moveUp(char[][] matrix, int row, int col) {
+        matrix[row][col] = '.';
+        int newRow = row - 1;
+        updateCoordinates(matrix, newRow, col, "up");
+    }
+
+    private static void moveDown(char[][] matrix, int row, int col) {
+        matrix[row][col] = '.';
+        int newRow = row + 1;
+        updateCoordinates(matrix, newRow, col, "down");
+    }
+
+    private static void updateCoordinates(char[][] matrix, int row, int col, String direction) {
+        if (outsideOfBounds(matrix, row, col)) {
+            isLost = true;
+        } else if (matrix[row][col] == 'O') {
+            extraMoveFromBonus(direction, matrix, row, col);
+        } else if (matrix[row][col] == 'f') {
+            flowersCount += 1;
+            matrix[row][col] = 'B';
+
+            beeRow = row;
+            beeCol = col;
+        } else {
+            matrix[row][col] = 'B';
+            beeRow = row;
+            beeCol = col;
+        }
+    }
+
+    private static void extraMoveFromBonus(String direction, char[][] matrix, int row, int col) {
+        moveBee(matrix, direction, row, col);
+    }
+
+    private static boolean outsideOfBounds(char[][] matrix, int row, int col) {
+        return row < 0 || row >= matrix.length || col < 0 || col >= matrix[row].length;
+    }
+
+    private static void findBeeCoordinates(char[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix.length; col++) {
+                if (matrix[row][col] == 'B') {
+                    beeRow = row;
+                    beeCol = col;
+                    break;
+                }
+            }
+        }
+    }
+
+    private static char[][] readMatrix(Scanner scanner, int n) {
+        char[][] matrix = new char[n][n];
+
+        for (int i = 0; i < matrix.length; i++) {
+            String line = scanner.nextLine();
+            matrix[i] = line.replaceAll("\\s+", "").toCharArray();
+        }
+        return matrix;
+    }
+
+    public static boolean beeIsLost() {
+        return isLost;
     }
 
     private static void printMatrix(char[][] matrix) {
+        if (beeIsLost()) {
+            System.out.println("The bee got lost!");
+        }
+
+        if (flowersCount >= NEEDED_FLOWERS) {
+            System.out.printf("Great job, the bee manage to pollinate %d flowers!\n", flowersCount);
+        } else {
+            System.out.printf("The bee couldn't pollinate the flowers, she needed %d flowers more\n", NEEDED_FLOWERS - flowersCount);
+        }
+
         for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix[row].length; col++) {
-                System.out.print(matrix[row][col]);
+                System.out.print(matrix[row][col] + " ");
             }
             System.out.println();
         }
     }
-
-    private static void moveLeft(int newRow, int newCol, char[][] matrix) {
-        if (outSideOfBounds(newRow, newCol, matrix)) {
-            System.out.println("The bee got lost!");
-            gotLost = true;
-            return;
-        }
-        if (matrix[newRow][newCol] == '.') {
-            matrix[newRow][newCol] = 'B';
-            matrix[beeRow][beeCol] = '.';
-            beeCol = newCol;
-        } else if (matrix[newRow][newCol] == 'f') {
-            flowers++;
-            matrix[beeRow][beeCol] = '.';
-            matrix[newRow][newCol] = 'B';
-            beeCol = newCol;
-        } else if (matrix[newRow][newCol] == 'O') {
-            matrix[newRow][newCol] = '.';
-            matrix[beeRow][beeCol] = '.';
-            beeCol -= 2;
-            if (matrix[beeRow][beeCol] == 'f') {
-                flowers++;
-            }
-            matrix[beeRow][beeCol] = 'B';
-        }
-    }
-
-    private static void moveRight(int newRow, int newCol, char[][] matrix) {
-        if (outSideOfBounds(newRow, newCol, matrix)) {
-            System.out.println("The bee got lost!");
-            gotLost = true;
-            return;
-        }
-
-        if (matrix[newRow][newCol] == '.') {
-            matrix[newRow][newCol] = 'B';
-            matrix[beeRow][beeCol] = '.';
-            beeCol = newCol;
-        } else if (matrix[newRow][newCol] == 'f') {
-            flowers++;
-            matrix[beeRow][beeCol] = '.';
-            matrix[newRow][newCol] = 'B';
-            beeCol = newCol;
-        } else if (matrix[newRow][newCol] == 'O') {
-            matrix[newRow][newCol] = '.';
-            matrix[beeRow][beeCol] = '.';
-            beeCol += 2;
-            if (matrix[beeRow][beeCol] == 'f') {
-                flowers++;
-            }
-            matrix[beeRow][beeCol] = 'B';
-        }
-    }
-
-    private static void moveDown(int newRow, int newCol, char[][] matrix) {
-        if (outSideOfBounds(newRow, newCol, matrix)) {
-            System.out.println("The bee got lost!");
-            gotLost = true;
-            return;
-        }
-
-        if (matrix[newRow][newCol] == '.') {
-            matrix[newRow][newCol] = 'B';
-            matrix[beeRow][beeCol] = '.';
-            beeRow = newRow;
-        } else if (matrix[newRow][newCol] == 'f') {
-            flowers++;
-            matrix[beeRow][beeCol] = '.';
-            matrix[newRow][newCol] = 'B';
-            beeRow = newRow;
-        } else if (matrix[newRow][newCol] == 'O') {
-            matrix[beeRow][beeCol] = '.';
-            matrix[newRow][newCol] = '.';
-            beeRow += 2;
-            if (matrix[beeRow][beeCol] == 'f') {
-                flowers++;
-            }
-            matrix[beeRow][beeCol] = 'B';
-        }
-    }
-
-
-    private static void moveBeeUp(int newRow, int newCol, char[][] matrix) {
-        if (outSideOfBounds(newRow, newCol, matrix)) {
-            System.out.println("The bee got lost!");
-            gotLost = true;
-            return;
-        }
-
-        if (matrix[newRow][newCol] == '.') {
-            matrix[newRow][newCol] = 'B';
-            matrix[beeRow][beeCol] = '.';
-            beeRow = newRow;
-        } else if (matrix[newRow][newCol] == 'f') {
-            flowers++;
-            matrix[beeRow][beeCol] = '.';
-            matrix[newRow][newCol] = 'B';
-            beeRow = newRow;
-        } else if (matrix[newRow][newCol] == 'O') {
-            matrix[newRow][newCol] = '.';
-            matrix[beeRow][beeCol] = '.';
-            beeRow -= 2;
-            if (matrix[beeRow][beeCol] == 'f') {
-                flowers++;
-            }
-            matrix[beeRow][beeCol] = 'B';
-        }
-    }
-
-    private static boolean outSideOfBounds(int newRol, int newCol, char[][] matrix) {
-        return newRol < 0 || newRol >= matrix.length || newCol < 0 || newCol >= matrix.length;
-    }
 }
-
-
-
-
 
 
 

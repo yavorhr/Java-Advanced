869fd245b1fb026f2edd754341e6819cc8d71068
wolfie <yavorhristozov@gmail.com>
2024-01-28 21:15:16 +0200

    refact : Rabbit class

--- 11. Exams Practice/3. Classes/Rabbits/Rabbits/Rabbit.java
+++ 11. Exams Practice/3. Classes/Rabbits/Rabbits/Rabbit.java
@@ -7,27 +7,37 @@ public class Rabbit {
         this.name = name;
         this.species = species;
         this.available = true;
-
     }
 
     public String getName() {
         return name;
     }
 
+    public Rabbit setName(String name) {
+        this.name = name;
+        return this;
+    }
+
     public String getSpecies() {
-        return this.species;
+        return species;
+    }
+
+    public Rabbit setSpecies(String species) {
+        this.species = species;
+        return this;
     }
 
     public boolean isAvailable() {
-        return this.available;
+        return available;
     }
 
-    public void setAvailable(boolean available) {
+    public Rabbit setAvailable(boolean available) {
         this.available = available;
+        return this;
     }
 
     @Override
     public String toString() {
-        return String.format("Rabbit (%s): %s", this.species, this.name);
+        return String.format("Rabbit (%s): %s",this.species, this.name);
     }
 }

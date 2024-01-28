ca140f828d3002e935098192e72b8d09dea280b3
wolfie <yavorhristozov@gmail.com>
2024-01-28 20:33:39 +0200

    refact : Main

--- 11. Exams Practice/3. Classes/Parking/Parking/Main.java
+++ 11. Exams Practice/3. Classes/Parking/Parking/Main.java
@@ -1,14 +1,21 @@
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Initialize the repository
         Parking parking = new Parking("Underground parking garage", 5);
 
+        // Initialize entity
         Car volvo = new Car("Volvo", "XC70", 2010);
 
+        // Print Car
         System.out.println(volvo); // Volvo XC70 (2010)
 
+        // Add Car
         parking.add(volvo);
 
+        // Remove Car
         System.out.println(parking.remove("Volvo", "XC90")); // false
         System.out.println(parking.remove("Volvo", "XC70")); // true
 
@@ -18,12 +25,21 @@ public class Main {
         parking.add(peugeot);
         parking.add(audi);
 
+        // Get Latest Car
         Car latestCar = parking.getLatestCar();
         System.out.println(latestCar); // Peugeot 307 (2011)
 
+        // Get Car
         Car audiS4 = parking.getCar("Audi", "S4");
         System.out.println(audiS4); // Audi S4 (2005)
+
+        // Count
         System.out.println(parking.getCount()); // 2
+
+        // Get Statistics
         System.out.println(parking.getStatistics());
+        // The cars are parked in Underground parking garage:
+        // Peugeot 307 (2011)
+        // Audi S4 (2005)
     }
 }

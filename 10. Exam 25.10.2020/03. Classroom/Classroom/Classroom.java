3a70022d34ecfb327297565965707131cbd2d28d
wolfie <yavorhristozov@gmail.com>
2024-01-29 11:38:40 +0200

    refact : Classroom class

--- 10. Exam 25.10.2020/03. Classroom/Classroom/Classroom.java
+++ 10. Exam 25.10.2020/03. Classroom/Classroom/Classroom.java
@@ -1,11 +1,10 @@
-package classroom;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Classroom {
-    public List<Student> students;
-    public int capacity;
+    private int capacity;
+    private List<Student> students;
 
     public Classroom(int capacity) {
         this.capacity = capacity;
@@ -13,11 +12,11 @@ public class Classroom {
     }
 
     public int getCapacity() {
-        return this.capacity;
+        return capacity;
     }
 
     public List<Student> getStudents() {
-        return this.students;
+        return students;
     }
 
     public int getStudentCount() {
@@ -25,59 +24,67 @@ public class Classroom {
     }
 
     public String registerStudent(Student student) {
-        if (this.students.size() < this.capacity && !this.students.contains(student)) {
-            StringBuilder sb = new StringBuilder("Added student ");
-            students.add(student);
-            sb.append(student.getFirstName()).append(" ").append(student.getLastName());
-            return sb.toString();
-        } else if (this.students.contains(student)) {
-            return "Student is already in the classroom";
+        String output = "";
+
+        if (studentIsInClassroom(student)) {
+            output = "Student is already in the classroom";
+        } else if (this.capacity < this.getStudentCount()) {
+            output = "No seats in the classroom";
+        } else {
+            this.students.add(student);
+            output = String.format("Added student %s %s", student.getFirstName(), student.getLastName());
         }
-        return "No seats in the classroom";
+
+        return output;
     }
 
     public String dismissStudent(Student student) {
-        if (this.students.contains(student)) {
-            students.remove(student);
-            return String.format("Removed student %s %s", student.getFirstName(), student.getLastName());
+        String output = "";
+
+        if (!studentIsInClassroom(student)) {
+            output = "Student not found";
+        } else {
+            this.students.remove(student);
+            output = String.format("Removed student %s %s", student.getFirstName(), student.getLastName());
         }
-        return "Student not found";
+
+        return output;
     }
 
     public String getSubjectInfo(String subject) {
-        StringBuilder sb = new StringBuilder("Subject: ");
-        for (Student student : this.students) {
-            if (student.getBestSubject().equals(subject)) {
-                sb.append(subject).append(System.lineSeparator()).append("Students:").append(System.lineSeparator());
-                sb.append(student.getFirstName()).append(" ").append(student.getLastName()).append(System.lineSeparator());
-            }
-            return sb.toString().trim();
-        }
-        return "No students enrolled for the subject";
+        List<Student> filteredBySubject = students.stream().filter(s -> s.getBestSubject().equals(subject)).collect(Collectors.toList());
 
+        if (filteredBySubject.size() == 0) {
+            return "No students enrolled for the subject";
+        } else {
+            StringBuilder sb = new StringBuilder(String.format("Subject: %s\nStudents:\n", subject));
+
+            filteredBySubject.forEach(s -> sb.append(String.format("%s %s\n", s.getFirstName(), s.getLastName())));
+            return sb.toString();
+        }
     }
 
-    public String getStudent(String firstName, String lastName) {
-        String student = "";
-        for (Student currentStudent : this.students) {
-            if (currentStudent.getFirstName().equals(firstName) && currentStudent.getLastName().equals(lastName)) {
-                student = currentStudent.toString();
-            }
-        }
-        return student;
+    public Student getStudent(String firstName, String lastName) {
+        return this.students.stream()
+                .filter(s ->
+                        s.getFirstName().equals(firstName) && s.getLastName().equals(lastName))
+                .collect(Collectors.toList())
+                .get(0);
     }
 
     public String getStatistics() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(String.format("Classroom size: %d%n", students.size()));
-        for (Student student : students) {
-            sb.append(String.format("==%s", student.toString())).append(System.lineSeparator());
-        }
-        return sb.toString().trim();
+        StringBuilder sb = new StringBuilder(String.format("Classroom size: %d\n", this.getStudentCount()));
+
+        this.students.forEach(s ->
+                sb.append(String.format("==Student: First Name= %s , Last Name= %s , Best Subject= %s\n",
+                        s.getFirstName(),
+                        s.getLastName(),
+                        s.getBestSubject())));
+
+        return sb.toString();
+    }
+
+    private boolean studentIsInClassroom(Student student) {
+        return this.students.contains(student);
     }
 }
-
-//o	"Classroom size: {added_students_count}
-//   ==Student: First Name= {firstName} , Last Name= {lastName} , Best Subject= {bestSubject}
-//   ==Student: First Name= {firstName} , Last Name= {lastName} , Best Subject= {bestSubject}
-//   (…)"

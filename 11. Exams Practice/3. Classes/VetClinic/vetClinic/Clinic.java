4908807174debf58e40c060626300770fd2d84e1
wolfie <yavorhristozov@gmail.com>
2024-01-29 11:03:55 +0200

    refact : Clinic class
    Implement use of Stream API instead of for cycles

--- 11. Exams Practice/3. Classes/VetClinic/vetClinic/Clinic.java
+++ 11. Exams Practice/3. Classes/VetClinic/vetClinic/Clinic.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+
 public class Clinic {
-    private List<Pet> data;
     private int capacity;
+    private List<Pet> data;
 
     public Clinic(int capacity) {
         this.capacity = capacity;
@@ -11,44 +12,36 @@ public class Clinic {
     }
 
     public void add(Pet pet) {
-        if (data.size() < capacity) {
-            data.add(pet);
+        if (this.capacity > this.getCount()) {
+            this.data.add(pet);
         }
     }
 
     public boolean remove(String name) {
-        for (Pet pet : data) {
-            if (pet.getName().equals(name)) {
-                data.remove(pet);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public Pet getOldestPet() {
-        return this.data.stream().max(Comparator.comparing(Pet::getAge)).get();
-    }
-
-    public Pet getPet(String name, String owner) {
-        for (Pet pet : data) {
-            if (pet.getName().equals(name) && pet.getOwner().equals(owner)) {
-              return pet;
-            }
-        }
-        return null;
+        return this.data.removeIf(p -> p.getName().equals(name));
     }
 
     public int getCount() {
-        return data.size();
+        return this.data.size();
+    }
+
+    public Pet getPet(String name, String owner) {
+        return this.data.stream()
+                .filter(p ->
+                        p.getName().equals(name) && p.getOwner().equals(owner))
+                .collect(Collectors.toList()).get(0);
+    }
+
+    public Pet getOldestPet() {
+        return this.data.stream()
+                .sorted((f, s) -> Integer.compare(s.getAge(), f.getAge()))
+                .collect(Collectors.toList()).get(0);
     }
 
     public String getStatistics() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("The clinic has the following patients:").append(System.lineSeparator());
-        for (Pet pet : data) {
-            sb.append(String.format("%s %s",pet.getName(),pet.getOwner())).append(System.lineSeparator());
-        }
-        return sb.toString().trim();
+        StringBuilder sb = new StringBuilder("The clinic has the following patients:\n");
+        this.data.forEach(p -> sb.append(String.format("%s %s", p.getName(), p.getOwner())).append(System.lineSeparator()));
+
+        return sb.toString();
     }
 }

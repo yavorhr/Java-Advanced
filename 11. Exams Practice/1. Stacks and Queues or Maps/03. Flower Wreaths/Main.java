f0c3019572f2545fa4859ff7ad7d7d5bc376dad9
wolfie <yavorhristozov@gmail.com>
2024-01-29 20:49:54 +0200

    refact : 03. Flowers Wreaths
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 11. Exams Practice/1. Stacks and Queues or Maps/03. Flower Wreaths/Main.java
+++ 11. Exams Practice/1. Stacks and Queues or Maps/03. Flower Wreaths/Main.java
@@ -1,49 +1,64 @@
 import java.util.ArrayDeque;
 import java.util.Arrays;
-
 import java.util.Scanner;
 
 public class Main {
+    public static int wreaths = 0;
+    public static int storedFlowers = 0;
+    public static final int REQUIRED_WREATHS = 5;
+    public static final int VALUE_TO_MATCH = 15;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayDeque<Integer> liliesStack = new ArrayDeque<>();
-        ArrayDeque<Integer> rosesQueue = new ArrayDeque<>();
-
-        Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).forEach(liliesStack::push);
-        Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).forEach(rosesQueue::offer);
-
-        int flowersRest = 0;
-        int wreaths = 0;
+        ArrayDeque<Integer> liliesStack = readDeque(scanner, "stack");
+        ArrayDeque<Integer> rosesQueue = readDeque(scanner, "queue");
 
         while (!liliesStack.isEmpty() && !rosesQueue.isEmpty()) {
-            int lilly = liliesStack.peek(); //13
-            int rose = rosesQueue.peek();   //2
-            int result = lilly + rose;
+            int lilly = liliesStack.peek();
+            int rose = rosesQueue.peek();
+            int sum = lilly + rose;
 
-            if (result == 15) {
-                wreaths++;
-                liliesStack.pop();
-                rosesQueue.poll();
-            } else if (result > 15) {
-                int num = liliesStack.pop();
-                int numToAdd = num - 2;
-                liliesStack.push(numToAdd);
-            } else {
-                int flowersToStore = liliesStack.pop() + rosesQueue.poll();
-                flowersRest += flowersToStore;
-            }
+            combineFlowers(liliesStack, rosesQueue, sum);
         }
 
-        int additionalWreaths = flowersRest / 15;
-        wreaths += additionalWreaths;
+        wreaths += storedFlowers / VALUE_TO_MATCH;
+        printOutput();
+    }
 
+    private static void printOutput() {
         if (wreaths >= 5) {
-            System.out.println(String.format("You made it, you are going to the competition with %d wreaths!",wreaths));
+            System.out.printf("You made it, you are going to the competition with %d wreaths!\n", wreaths);
         } else {
-            int notEnoughWreaths = 5-wreaths;
-            System.out.println(String.format("You didn't make it, you need %d wreaths more!",notEnoughWreaths));
+            System.out.printf("You didn't make it, you need %d wreaths more!\n", REQUIRED_WREATHS - wreaths);
         }
-        
+    }
+
+    private static void combineFlowers(ArrayDeque<Integer> liliesStack, ArrayDeque<Integer> rosesQueue, int sum) {
+        if (sum == VALUE_TO_MATCH) {
+            wreaths++;
+            removeOneFlowerPerDeck(liliesStack, rosesQueue);
+        } else if (sum > VALUE_TO_MATCH) {
+            int poppedValue = liliesStack.pop();
+            liliesStack.push(poppedValue - 2);
+        } else {
+            storedFlowers += sum;
+            removeOneFlowerPerDeck(liliesStack, rosesQueue);
+        }
+    }
+
+    private static void removeOneFlowerPerDeck(ArrayDeque<Integer> liliesStack, ArrayDeque<Integer> rosesQueue) {
+        liliesStack.pop();
+        rosesQueue.poll();
+    }
+
+    public static ArrayDeque<Integer> readDeque(Scanner scanner, String type) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        if (type.equals("stack")) {
+            Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).forEach(deque::push);
+        } else {
+            Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).forEach(deque::offer);
+        }
+        return deque;
     }
 }

5c6fe9e702682f64d80cbd603e7b1fe08255bb20
wolfie <yavorhristozov@gmail.com>
2024-01-29 16:59:43 +0200

    refact : 02 Dating App
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 11. Exams Practice/1. Stacks and Queues or Maps/02. Dating App/Main.java
+++ 11. Exams Practice/1. Stacks and Queues or Maps/02. Dating App/Main.java
@@ -1,119 +1,88 @@
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
+    public static int MATCHES = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] malesInput = scanner.nextLine().split("\\s+");
-        String[] femalesInput = scanner.nextLine().split("\\s+");
-
         ArrayDeque<Integer> maleStack = new ArrayDeque<>();
+        Arrays.stream(readArray(scanner)).forEach(maleStack::push);
+
         ArrayDeque<Integer> femaleQueue = new ArrayDeque<>();
+        Arrays.stream(readArray(scanner)).forEach(femaleQueue::offer);
 
-        int counter = 0;
+        while (!maleStack.isEmpty() && !femaleQueue.isEmpty()) {
+            int maleValue = maleStack.peek();
+            int femaleValue = femaleQueue.peek();
 
-        //fulfill queue of males
-        for (int i = 0; i < malesInput.length; i++) {
-            maleStack.push(Integer.parseInt(malesInput[i])); //add first
-        }
-
-        //fulfill stack of females
-        for (int i = 0; i < femalesInput.length; i++) {
-            femaleQueue.add(Integer.parseInt(femalesInput[i])); //add last
-        }
-
-        while (maleStack.size() > 0 && femaleQueue.size() > 0) {
-            int male = maleStack.peek();
-            int female = femaleQueue.peek();
-            if (male > 0 && female > 0) {
-                if (male % 25 == 0 || female % 25 == 0) {
-
-                    if (male % 25 == 0) {
-                        if (maleStack.size() > 1) {
-                            maleStack.pop();
-                            maleStack.pop();
-                        } else {
-                            maleStack.pop();
-                        }
-                    }
-
-                    if (female % 25 == 0) {
-                        if (femaleQueue.size() > 1) {
-                            femaleQueue.remove();
-                            femaleQueue.remove();
-                        } else {
-                            femaleQueue.remove();
-                        }
-                    }
-                    continue;
+            if (valuesGreaterThanZero(maleValue, femaleValue) && valuesNotDivisibleBy25(maleValue, femaleValue)) {
+                if (maleValue == femaleValue) {
+                    MATCHES++;
+                    maleStack.pop();
+                    femaleQueue.poll();
+                } else {
+                    femaleQueue.poll();
+                    int poppedValue = maleStack.pop();
+                    maleStack.push(poppedValue - 2);
                 }
+            } else if (!valuesGreaterThanZero(maleValue, femaleValue)) {
+                checkIfValueIsBelowZero(maleStack, maleValue, "stack");
+                checkIfValueIsBelowZero(femaleQueue, femaleValue, "queue");
+            } else if (!valuesNotDivisibleBy25(maleValue, femaleValue)) {
+                checkIfDivisibleBy25(maleStack, maleValue, "stack");
+                checkIfDivisibleBy25(femaleQueue, femaleValue, "queue");
             }
+        }
+        System.out.println(printResult(maleStack, femaleQueue));
+    }
 
-            if (male <= 0 || female <= 0) {
+    private static String printResult(ArrayDeque<Integer> maleStack, ArrayDeque<Integer> femaleQueue) {
+        StringBuilder sb = new StringBuilder(String.format("Matches: %d\n", MATCHES));
+        sb.append(String.format("Males left: %s\n", maleStack.isEmpty() ? "none" : getDeckValues(maleStack)));
+        sb.append(String.format("Females left: %s", femaleQueue.isEmpty() ? "none" : getDeckValues(femaleQueue)));
 
-                if (male <= 0) {
-                    maleStack.pop(); //removes first
-                }
-                if (female <= 0) {
-                    femaleQueue.remove(); //removes first
-                }
-                continue;
-            }
+        return sb.toString();
+    }
 
-            if (male == female) {
-                counter++;
-                maleStack.pop();
-                femaleQueue.remove();
+    private static String getDeckValues(ArrayDeque<Integer> deck) {
+        return deck.stream().map(String::valueOf).collect(Collectors.joining(","));
+    }
+
+    public static boolean valuesGreaterThanZero(int firstValue, int secondValue) {
+        return firstValue > 0 && secondValue > 0;
+    }
+
+    public static boolean valuesNotDivisibleBy25(int firstValue, int secondValue) {
+        return firstValue % 25 != 0 && secondValue % 25 != 0;
+    }
+
+    private static void checkIfValueIsBelowZero(ArrayDeque<Integer> deck, int value, String type) {
+        if (value <= 0) {
+            if (type.equals("stack")) {
+                deck.pop();
             } else {
-                femaleQueue.remove();
-
-                male -= 2;
-                maleStack.pop();
-
-                ArrayDeque<Integer> setQueue = new ArrayDeque<>();
-                setQueue.add(male);
-                for (int i = 0; i < maleStack.size(); i++) {
-                    int current = maleStack.pop();
-                    setQueue.add(current);
-                    i--;
-                }
-                maleStack = setQueue;
+                deck.poll();
             }
         }
+    }
 
-        System.out.println("Matches: " + counter);
-
-        if (maleStack.size() > 0) {
-            System.out.print("Males left: ");
-
-            for (int i = 0; i < maleStack.size(); i++) {
-                if (i == maleStack.size() - 1) {
-                    System.out.println(maleStack.pop());
-                } else {
-                    System.out.print(maleStack.pop() + ", ");
-                    i--;
-                }
-            }
-            System.out.println("Females left: none");
-
-        } else if (maleStack.isEmpty() && femaleQueue.isEmpty()) {
-            System.out.println("Males left: none");
-            System.out.println("Females left: none");
-        } else if (maleStack.isEmpty()) {
-            System.out.println("Males left: none");
-
-
-            System.out.print("Females left: ");
-
-            for (int i = 0; i < femaleQueue.size(); i++) {
-                if (i == femaleQueue.size() - 1) {
-                    System.out.println(femaleQueue.remove());
-                } else {
-                    System.out.print(femaleQueue.remove() + ", ");
-                    i--;
-                }
+    private static void checkIfDivisibleBy25(ArrayDeque<Integer> deck, int value, String type) {
+        if (value % 25 == 0) {
+            if (type.equals("stack")) {
+                deck.pop();
+                deck.pop();
+            } else {
+                deck.poll();
+                deck.poll();
             }
         }
+    }
+
+    public static int[] readArray(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
     }
 }

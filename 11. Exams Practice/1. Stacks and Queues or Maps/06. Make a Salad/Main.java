edf9b326d43eaa6affabb9e20497d7e6cf01b08c
wolfie <yavorhristozov@gmail.com>
2024-01-31 19:13:30 +0200

    refact : 02. Make a Salad
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 11. Exams Practice/1. Stacks and Queues or Maps/06. Make a Salad/Main.java
+++ 11. Exams Practice/1. Stacks and Queues or Maps/06. Make a Salad/Main.java
@@ -1,40 +1,60 @@
 import java.util.*;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Map<String, Integer> veggies = new LinkedHashMap<String, Integer>() {{
-            put("tomato", 80);
-            put("carrot", 136);
-            put("lettuce", 109);
-            put("potato", 215);
-        }};
+        ArrayDeque<String> vegetablesQueue = readStringDeque(scanner);
+        ArrayDeque<Integer> saladsStack = readIntegerDeque(scanner);
 
-        ArrayDeque<Integer> saladsCal = new ArrayDeque<>();
-        ArrayDeque<String> vegetables = new ArrayDeque<>();
+        Map<String, Integer> kcalPerVegetable = initMap();
 
-        Arrays.stream(scanner.nextLine().split("\\s+")).forEach(vegetables::offer);
-        Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).forEach(saladsCal::push);
+        while (!saladsStack.isEmpty() && !vegetablesQueue.isEmpty()) {
+            int currentSaladCal = saladsStack.pop();
 
-        while (!vegetables.isEmpty() && !saladsCal.isEmpty()) {
-            int currentSalad = saladsCal.peek();
-
-            while (currentSalad > 0 && !vegetables.isEmpty()) {
-                currentSalad -= veggies.get(vegetables.poll());
+            while (currentSaladCal > 0 && !vegetablesQueue.isEmpty()) {
+                String currentVegetable = vegetablesQueue.poll();
+                currentSaladCal -= kcalPerVegetable.get(currentVegetable);
             }
 
-            System.out.print(saladsCal.pop() + " ");
-        }
-        System.out.println();
-
-        if (!saladsCal.isEmpty()) {
-            System.out.println(saladsCal.toString().replaceAll("[\\[\\],]", ""));
+            System.out.print(currentSaladCal + " ");
         }
 
-        if (!vegetables.isEmpty()) {
-            System.out.println(vegetables.toString().replaceAll("[\\[\\],]", ""));
+        printOutput(vegetablesQueue, saladsStack);
+    }
+
+    private static void printOutput(ArrayDeque<String> vegetablesQueue, ArrayDeque<Integer> saladsStack) {
+        if (!saladsStack.isEmpty()) {
+            System.out.println(saladsStack.toString().replaceAll("[\\[\\],]", ""));
+        }
+
+        if (!vegetablesQueue.isEmpty()) {
+            System.out.println(vegetablesQueue.toString().replaceAll("[\\[\\],]", ""));
         }
     }
-}
 
+    private static Map<String, Integer> initMap() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("tomato", 80);
+        map.put("carrot", 136);
+        map.put("lettuce", 109);
+        map.put("potato", 215);
+
+        return map;
+    }
+
+
+    public static ArrayDeque<String> readStringDeque(Scanner scanner) {
+        ArrayDeque<String> deque = new ArrayDeque<>();
+        Arrays.stream(scanner.nextLine().split(" ")).forEach(deque::offer);
+
+        return deque;
+    }
+
+    public static ArrayDeque<Integer> readIntegerDeque(Scanner scanner) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).forEach(deque::push);
+
+        return deque;
+    }
+}
